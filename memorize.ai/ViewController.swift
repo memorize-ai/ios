dd14f146ea2daa64ff1c7910fcd128d79d10f4f4
Ken Mueller <kenmueller0@gmail.com>
2019-01-31 22:58:38 -0800

    Initial Commit

---
+++ memorize.ai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  memorize.ai
+//
+//  Created by Ken Mueller on 1/31/19.
+//  Copyright © 2019 Ken Mueller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
