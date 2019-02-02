80e60ff856a18c602fb41d262dd9185545a964cd
Ken Mueller <kenmueller0@gmail.com>
2019-02-02 13:21:25 -0800

    sign in and sign up working

--- memorize.ai/Auth/SignInViewController.swift
+++ memorize.ai/Auth/SignInViewController.swift
@@ -1,18 +1,14 @@
 import UIKit
 import Firebase
 
-class SignUpViewController: UIViewController, UITextFieldDelegate {
-	@IBOutlet weak var nameTextField: UITextField!
-	@IBOutlet weak var nameBarView: UIView!
+class SignInViewController: UIViewController, UITextFieldDelegate {
 	@IBOutlet weak var emailTextField: UITextField!
 	@IBOutlet weak var emailBarView: UIView!
-	@IBOutlet weak var invalidEmailLabel: UILabel!
 	@IBOutlet weak var passwordTextField: UITextField!
 	@IBOutlet weak var passwordBarView: UIView!
-	@IBOutlet weak var passwordTooShortLabel: UILabel!
-	@IBOutlet weak var signUpButton: UIButton!
-	@IBOutlet weak var signUpButtonBottomConstraint: NSLayoutConstraint!
-	@IBOutlet weak var signUpActivityIndicator: UIActivityIndicatorView!
+	@IBOutlet weak var signInButton: UIButton!
+	@IBOutlet weak var signInButtonBottomConstraint: NSLayoutConstraint!
+	@IBOutlet weak var signInActivityIndicator: UIActivityIndicatorView!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -37,7 +33,7 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
 	
 	@objc func keyboardWillShow(notification: NSNotification) {
 		if let height = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height {
-			signUpButtonBottomConstraint.constant = height
+			signInButtonBottomConstraint.constant = height
 			UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseOut, animations: {
 				self.view.layoutIfNeeded()
 			}, completion: nil)
@@ -45,7 +41,7 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
 	}
 	
 	@objc func keyboardWillHide(notification: NSNotification) {
-		signUpButtonBottomConstraint.constant = 145
+		signInButtonBottomConstraint.constant = 145
 		UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveLinear, animations: {
 			self.view.layoutIfNeeded()
 		}, completion: nil)
@@ -53,7 +49,6 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
 	
 	func textFieldDidBeginEditing(_ textField: UITextField) {
 		switch textField {
-		case nameTextField: enable(barView: nameBarView)
 		case emailTextField: enable(barView: emailBarView)
 		case passwordTextField: enable(barView: passwordBarView)
 		default: return
@@ -62,76 +57,65 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
 	
 	func textFieldDidEndEditing(_ textField: UITextField) {
 		switch textField {
-		case nameTextField: disable(barView: nameBarView)
 		case emailTextField: disable(barView: emailBarView)
 		case passwordTextField: disable(barView: passwordBarView)
 		default: return
 		}
 	}
 	
-	@IBAction func nameTextFieldChanged() {
-		updateSignUpButton()
+	@IBAction func textFieldChanged() {
+		guard let emailText = emailTextField.text?.trim(), let passwordText = passwordTextField.text?.trim() else { return }
+		!(emailText.isEmpty || passwordText.isEmpty) ? enable() : disable()
 	}
 	
-	@IBAction func emailTextFieldChanged() {
-		guard let emailText = emailTextField.text?.trim() else { return }
-		invalidEmailLabel.isHidden = emailText.checkEmail() || emailText.isEmpty
-		updateSignUpButton()
-	}
-	
-	@IBAction func passwordTextFieldChanged() {
-		guard let passwordText = passwordTextField.text?.trim() else { return }
-		passwordTooShortLabel.isHidden = passwordText.count >= 6 || passwordText.isEmpty
-		updateSignUpButton()
-	}
-	
-	@IBAction func signUp() {
-		guard let nameText = nameTextField.text?.trim(), let emailText = emailTextField.text?.trim(), let passwordText = passwordTextField.text?.trim() else { return }
+	@IBAction func signIn() {
+		guard let emailText = emailTextField.text?.trim(), let passwordText = passwordTextField.text?.trim() else { return }
 		showActivityIndicator()
 		dismissKeyboard()
-		Auth.auth().createUser(withEmail: emailText, password: passwordText) { authResult, error in
+		Auth.auth().signIn(withEmail: emailText, password: passwordText) { user, error in
 			if error == nil {
-				id = authResult?.user.uid
-				ref.child("users/\(id!)").setValue(["name": nameText, "email": emailText])
-				name = nameText
-				saveLogin(email: emailText, password: passwordText)
-				loadData()
-				self.hideActivityIndicator()
-				self.performSegue(withIdentifier: "signUp", sender: self)
+				id = user?.user.uid
+				ref.child("users/\(id!)/name").observeSingleEvent(of: .value) { snapshot in
+					name = snapshot.value as? String
+					saveLogin(email: emailText, password: passwordText)
+					loadData()
+					self.hideActivityIndicator()
+					self.performSegue(withIdentifier: "signIn", sender: self)
+				}
 			} else if let error = error {
 				self.hideActivityIndicator()
 				switch error.localizedDescription {
 				case "Network error (such as timeout, interrupted connection or unreachable host) has occurred.":
 					self.showAlert("No internet")
 				default:
-					self.showAlert("There was a problem creating a new account")
+					self.showAlert("Invalid email/password")
 				}
 			}
 		}
 	}
 	
 	func showActivityIndicator() {
-		signUpButton.isEnabled = false
-		signUpButton.setTitle(nil, for: .normal)
-		signUpActivityIndicator.startAnimating()
+		signInButton.isEnabled = false
+		signInButton.setTitle(nil, for: .normal)
+		signInActivityIndicator.startAnimating()
 	}
 	
 	func hideActivityIndicator() {
-		signUpButton.isEnabled = true
-		signUpButton.setTitle("SIGN UP", for: .normal)
-		signUpActivityIndicator.stopAnimating()
+		signInButton.isEnabled = true
+		signInButton.setTitle("SIGN UP", for: .normal)
+		signInActivityIndicator.stopAnimating()
 	}
 	
 	func enable() {
-		signUpButton.isEnabled = true
-		signUpButton.setTitleColor(#colorLiteral(red: 0, green: 0.5694751143, blue: 1, alpha: 1), for: .normal)
-		signUpButton.backgroundColor = .white
+		signInButton.isEnabled = true
+		signInButton.setTitleColor(UIColor(red: 72 / 255, green: 204 / 255, blue: 127 / 255, alpha: 1), for: .normal)
+		signInButton.backgroundColor = .white
 	}
 	
 	func disable() {
-		signUpButton.isEnabled = false
-		signUpButton.setTitleColor(#colorLiteral(red: 0.9411764706, green: 0.9411764706, blue: 0.9411764706, alpha: 1), for: .normal)
-		signUpButton.backgroundColor = #colorLiteral(red: 0.862745098, green: 0.862745098, blue: 0.862745098, alpha: 1)
+		signInButton.isEnabled = false
+		signInButton.setTitleColor(UIColor(red: 240 / 255, green: 240 / 255, blue: 240 / 255, alpha: 1), for: .normal)
+		signInButton.backgroundColor = UIColor(red: 220 / 255, green: 220 / 255, blue: 220 / 255, alpha: 1)
 	}
 	
 	func enable(barView: UIView) {
@@ -146,11 +130,6 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
 			barView.transform = .identity
 			barView.backgroundColor = .lightGray
 		}, completion: nil)
-	}
-	
-	func updateSignUpButton() {
-		guard let nameText = nameTextField.text?.trim(), let emailText = emailTextField.text?.trim(), let passwordText = passwordTextField.text?.trim() else { return }
-		!nameText.isEmpty && emailText.checkEmail() && passwordText.count >= 6 ? enable() : disable()
 	}
 	
 	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
