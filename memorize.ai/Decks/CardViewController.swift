2d39b0188516382d1d72f499cee434b5dc86e91e
Ken Mueller <kenmueller0@gmail.com>
2019-02-04 14:11:29 -0800

    cardVC done

--- memorize.ai/Decks/CardViewController.swift
+++ memorize.ai/Decks/CardViewController.swift
@@ -1,9 +1,33 @@
 import UIKit
 
 class CardViewController: UIViewController {
+	@IBOutlet weak var cardView: UIView!
+	@IBOutlet weak var titleBar: UIView!
+	@IBOutlet weak var frontLabel: UILabel!
+	@IBOutlet weak var backLabel: UILabel!
+	
 	var card: Card?
 	
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		titleBar.roundCorners(corners: [.topLeft, .topRight], radius: 10)
+		frontLabel.text = card?.front
+		backLabel.text = card?.back
+		cardView.transform = CGAffineTransform(scaleX: 0, y: 0)
+		UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseIn, animations: {
+			self.view.backgroundColor = UIColor.black.withAlphaComponent(0.4)
+			self.cardView.transform = .identity
+		}, completion: nil)
+	}
+	
+	@IBAction func hide() {
+		UIView.animate(withDuration: 0.2, animations: {
+			self.cardView.transform = CGAffineTransform(scaleX: 0, y: 0)
+			self.view.backgroundColor = .clear
+		}) { finished in
+			if finished {
+				self.view.removeFromSuperview()
+			}
+		}
+	}
 }
