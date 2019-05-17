8fa4c51f1878dc88ad2002d776ae63b7f12e1a5e
Ken Mueller <kenmueller0@gmail.com>
2019-05-16 21:28:10 -0700

    white nav bar status

--- memorize.ai/Home/EditProfileViewController.swift
+++ memorize.ai/Home/EditProfileViewController.swift
@@ -89,11 +89,13 @@ class EditProfileViewController: UIViewController, UINavigationControllerDelegat
 	}
 	
 	func uploadImage(_ image: UIImage, completion: @escaping () -> Void) {
+		profilePicture = image
 		if let data = image.pngData() {
 			let metadata = StorageMetadata()
 			metadata.contentType = "image/png"
 			storage.child("users/\(id!)").putData(data, metadata: metadata) { _, error in
 				guard error == nil else { return }
+				saveImage(image)
 				storage.child("users/\(id!)").downloadURL { url, error in
 					guard error == nil, let url = url else { return }
 					Auth.auth().currentUser?.createProfileChangeRequest().photoURL = url
