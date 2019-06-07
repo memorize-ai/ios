94a96e3cf4cb032a644287536322e7b385f4d01f
Ken Mueller <kenmueller0@gmail.com>
2019-06-07 12:50:54 -0700

    CardDraft

---
+++ memorize.ai/CardDraft.swift
@@ -0,0 +1,45 @@
+var cardDrafts = [CardDraft]()
+
+class CardDraft {
+	let id: String
+	let deckId: String
+	let cardId: String?
+	let type: CardDraftType
+	var front: String
+	var back: String
+	
+	init(id: String, deckId: String, cardId: String?, front: String, back: String) {
+		self.id = id
+		self.deckId = deckId
+		self.cardId = cardId
+		self.front = front
+		self.back = back
+		self.type = cardId == nil ? .new : .edit
+	}
+	
+	var deck: Deck? {
+		return Deck.get(deckId)
+	}
+	
+	var card: Card? {
+		guard let cardId = cardId else { return nil }
+		return Card.get(cardId, deckId: deckId)
+	}
+	
+	static func get(_ id: String) -> CardDraft? {
+		return cardDrafts.first { $0.id == id }
+	}
+	
+	static func get(deckId: String) -> CardDraft? {
+		return cardDrafts.first { $0.deckId == deckId }
+	}
+	
+	static func get(cardId: String) -> CardDraft? {
+		return cardDrafts.first { $0.cardId == cardId }
+	}
+}
+
+enum CardDraftType {
+	case new
+	case edit
+}
