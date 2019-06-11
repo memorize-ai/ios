8d35899a0c492352c9dd23fadf543cc2249ad617
Ken Mueller <kenmueller0@gmail.com>
2019-06-11 14:00:13 -0700

    clearAllData and removeDeck

--- memorize.ai/Decks/DeckSettingsViewController.swift
+++ memorize.ai/Decks/DeckSettingsViewController.swift
@@ -5,12 +5,18 @@ class DeckSettingsViewController: UIViewController, UITableViewDataSource, UITab
 	class DeckSetting {
 		let name: String
 		let color: UIColor
-		let action: (DeckSettingsViewController) -> () -> Void
+		let action: (DeckSettingsViewController) -> (DeckSettingTableViewCell) -> Void
+		var cell: DeckSettingTableViewCell?
 		
-		init(name: String, color: UIColor?, action: @escaping (DeckSettingsViewController) -> () -> Void) {
+		init(name: String, color: UIColor?, action: @escaping (DeckSettingsViewController) -> (DeckSettingTableViewCell) -> Void) {
 			self.name = name
 			self.color = color ?? .black
 			self.action = action
+		}
+		
+		func callAction(_ viewController: DeckSettingsViewController) {
+			guard let cell = cell else { return }
+			action(viewController)(cell)
 		}
 	}
 	
@@ -34,14 +40,17 @@ class DeckSettingsViewController: UIViewController, UITableViewDataSource, UITab
 		}
 	}
 	
-	func clearAllData() {
+	func clearAllData(_ cell: DeckSettingTableViewCell) {
+		guard let deck = deck else { return }
 		let alertController = UIAlertController(title: "Are you sure?", message: "All progress for this deck will be deleted. This cannot be undone", preferredStyle: .alert)
 		alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
 		alertController.addAction(UIAlertAction(title: "Clear", style: .default) { _ in
-			self.deck?.clearAllData { error in
+			deck.clearAllData { error in
 				if error == nil {
-					
+					cell.activityIndicator.stopAnimating()
+					buzz()
 				} else {
+					cell.activityIndicator.stopAnimating()
 					self.showAlert("An unknown error occurred. Please try again")
 				}
 			}
@@ -49,12 +58,22 @@ class DeckSettingsViewController: UIViewController, UITableViewDataSource, UITab
 		present(alertController, animated: true, completion: nil)
 	}
 	
-	func removeDeck() {
-		
+	func removeDeck(_ cell: DeckSettingTableViewCell) {
+		guard let deck = deck else { return }
+		firestore.document("decks/\(deck.id)").updateData(["hidden": true]) { error in
+			if error == nil {
+				cell.activityIndicator.stopAnimating()
+				buzz()
+				self.navigationController?.popViewController(animated: true)
+			} else {
+				cell.activityIndicator.stopAnimating()
+				self.showAlert("An unknown error occurred. Please try again")
+			}
+		}
 	}
 	
-	func deleteDeck() {
-		
+	func deleteDeck(_ cell: DeckSettingTableViewCell) {
+		guard let deck = deck else { return }
 	}
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -62,14 +81,21 @@ class DeckSettingsViewController: UIViewController, UITableViewDataSource, UITab
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+		let _cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+		guard let cell = _cell as? DeckSettingTableViewCell else { return _cell }
 		let element = filteredDeckSettings[indexPath.row]
-		cell.textLabel?.text = element.name
-		cell.textLabel?.textColor = element.color
+		cell.label.text = element.name
+		cell.label.textColor = element.color
+		element.cell = cell
 		return cell
 	}
 	
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-		filteredDeckSettings[indexPath.row].action(self)()
+		filteredDeckSettings[indexPath.row].callAction(self)
 	}
+}
+
+class DeckSettingTableViewCell: UITableViewCell {
+	@IBOutlet weak var label: UILabel!
+	@IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 }
