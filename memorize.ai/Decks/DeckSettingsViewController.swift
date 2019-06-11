30b8800a4eb599c36a82db63328da096f8933e14
Ken Mueller <kenmueller0@gmail.com>
2019-06-11 13:12:07 -0700

    table view for DeckSettingsVC

---
+++ memorize.ai/Decks/DeckSettingsViewController.swift
@@ -0,0 +1,64 @@
+import UIKit
+
+class DeckSettingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+	
+	class DeckSetting {
+		let name: String
+		let color: UIColor
+		let action: (DeckSettingsViewController) -> () -> Void
+		
+		init(name: String, color: UIColor?, action: @escaping (DeckSettingsViewController) -> () -> Void) {
+			self.name = name
+			self.color = color ?? .black
+			self.action = action
+		}
+	}
+	
+	let deckSettings = [
+		DeckSetting(name: "Clear all data", color: nil, action: clearAllData),
+		DeckSetting(name: "Remove deck", color: nil, action: removeDeck),
+		DeckSetting(name: "Permanently delete deck", color: .red, action: deleteDeck)
+	]
+	var deck: Deck?
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+	var filteredDeckSettings: [DeckSetting] {
+		switch deck?.role {
+		case .some(.owner):
+			return deckSettings
+		default:
+			return [deckSettings[0], deckSettings[1]]
+		}
+	}
+	
+	func clearAllData() {
+		
+	}
+	
+	func removeDeck() {
+		
+	}
+	
+	func deleteDeck() {
+		
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return filteredDeckSettings.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+		let element = filteredDeckSettings[indexPath.row]
+		cell.textLabel?.text = element.name
+		cell.textLabel?.textColor = element.color
+		return cell
+	}
+	
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		filteredDeckSettings[indexPath.row].action(self)()
+	}
+}
