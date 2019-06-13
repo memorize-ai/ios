1727f07fc409ec30aeba5baae11980c68878af19
Ken Mueller <kenmueller0@gmail.com>
2019-06-13 01:30:24 -0700

    delete ratings

--- memorize.ai/Ratings/DeckRatingsViewController.swift
+++ memorize.ai/Ratings/DeckRatingsViewController.swift
@@ -3,6 +3,8 @@ import UIKit
 class DeckRatingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 	@IBOutlet weak var ratingsTableView: UITableView!
 	
+	var cells = [Int : DeckRatingPreviewTableViewCell]()
+	
 	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 		super.prepare(for: segue, sender: self)
 		if let rateDeckVC = segue.destination as? RateDeckViewController, let rating = sender as? DeckRating {
@@ -13,11 +15,18 @@ class DeckRatingsViewController: UIViewController, UITableViewDataSource, UITabl
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
 		ChangeHandler.update { change in
-			if change == .deckRatingAdded || change == .deckRatingModified || change == .deckRatingRemoved {
+			if change == .deckModified || change == .deckRemoved || change == .deckRatingAdded || change == .deckRatingModified || change == .deckRatingRemoved {
 				self.ratingsTableView.reloadData()
 			}
 		}
 		updateCurrentViewController()
+	}
+	
+	func showConfirmAlert(_ message: String, completion: @escaping (Bool) -> Void) {
+		let alertController = UIAlertController(title: "Are you sure?", message: message, preferredStyle: .alert)
+		alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel) { _ in completion(false) })
+		alertController.addAction(UIAlertAction(title: "Delete", style: .destructive) { _ in completion(true) })
+		present(alertController, animated: true, completion: nil)
 	}
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -40,16 +49,63 @@ class DeckRatingsViewController: UIViewController, UITableViewDataSource, UITabl
 			cell.ratingLabel.font = UIFont(name: "Nunito-Bold", size: 40)
 			cell.detailLabel.text = "unrated"
 		}
+		cells[indexPath.row] = cell
 		return cell
 	}
 	
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		performSegue(withIdentifier: "editRating", sender: deckRatings[indexPath.row])
 	}
+	
+	func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+		guard editingStyle == .delete, let cell = cells[indexPath.row] else { return }
+		let deck = decks[indexPath.row]
+		if deck.hasRating {
+			let draftMessage = deck.hasRatingDraft ? " and draft" : ""
+			showConfirmAlert("Your published rating\(draftMessage) will be deleted. This action cannot be undone") { accepted in
+				if accepted {
+					cell.startLoading()
+					deck.unrate { error in
+						cell.stopLoading()
+						if error == nil {
+							self.ratingsTableView.reloadData()
+						} else {
+							self.showAlert("Unable to delete rating\(draftMessage). Please try again")
+						}
+					}
+				}
+			}
+		} else if let draft = deck.ratingDraft, let id = id {
+			showConfirmAlert("Your draft will be deleted. This action cannot be undone") { accepted in
+				if accepted {
+					cell.startLoading()
+					firestore.document("users/\(id)/ratingDrafts/\(draft.id)").delete { error in
+						cell.stopLoading()
+						if error == nil {
+							self.ratingsTableView.reloadData()
+						} else {
+							self.showAlert("Unable to delete draft. Please try again")
+						}
+					}
+				}
+			}
+		}
+	}
 }
 
 class DeckRatingPreviewTableViewCell: UITableViewCell {
 	@IBOutlet weak var deckNameLabel: UILabel!
 	@IBOutlet weak var ratingLabel: UILabel!
+	@IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 	@IBOutlet weak var detailLabel: UILabel!
+	
+	func startLoading() {
+		ratingLabel.isHidden = true
+		activityIndicator.startAnimating()
+	}
+	
+	func stopLoading() {
+		activityIndicator.stopAnimating()
+		ratingLabel.isHidden = false
+	}
 }
