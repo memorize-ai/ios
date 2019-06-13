497dc5ca95962da22175805050773b38f0f673cb
Ken Mueller <kenmueller0@gmail.com>
2019-06-12 18:10:02 -0700

    Rating.swift

---
+++ memorize.ai/Rating.swift
@@ -0,0 +1,21 @@
+var deckRatings = [DeckRating]()
+
+class DeckRating {
+	let id: String
+	var rating: Int
+	var review: String
+	
+	init(id: String, rating: Int, review: String) {
+		self.id = id
+		self.rating = rating
+		self.review = review
+	}
+	
+	var hasReview: Bool {
+		return !review.isEmpty
+	}
+	
+	var draft: RatingDraft? {
+		return RatingDraft.get(id)
+	}
+}
