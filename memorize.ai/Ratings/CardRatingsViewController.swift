9d916399070c22612f174d0acb5c6873b90b2c41
Ken Mueller <kenmueller0@gmail.com>
2019-06-14 02:28:31 -0700

    card ratings reload properly

--- memorize.ai/Ratings/CardRatingsViewController.swift
+++ memorize.ai/Ratings/CardRatingsViewController.swift
@@ -5,6 +5,12 @@ class CardRatingsViewController: UIViewController, UITableViewDataSource, UITabl
 	
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
+		ChangeHandler.update { change in
+			if change == .deckModified || change == .deckRemoved || change == .cardRemoved || change == .cardRatingAdded || change == .cardRatingModified || change == .cardRatingRemoved {
+				self.ratingsTableView.reloadData()
+			}
+		}
+		updateCurrentViewController()
 	}
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
