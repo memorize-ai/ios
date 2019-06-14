a3c76d41f0b46f1748d6eead8667197cdb245d73
Ken Mueller <kenmueller0@gmail.com>
2019-06-13 21:49:39 -0700

    Notification

---
+++ memorize.ai/Modals/Notification.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+extension UIViewController {
+	func showNotification(_ text: String, type: NotificationType, delay: Double = 2, completion: (() -> Void)? = nil) {
+		guard let notificationVC = storyboard?.instantiateViewController(withIdentifier: "notification") as? NotificationViewController else { return }
+		notificationVC.notificationView.backgroundColor = type.color
+		notificationVC.notificationLabel.text = text
+		addChild(notificationVC)
+		notificationVC.view.frame = view.frame
+		view.addSubview(notificationVC.view)
+		notificationVC.didMove(toParent: self)
+		notificationVC.show(delay, completion: completion)
+	}
+}
+
+enum NotificationType {
+	case success
+	case error
+	case normal
+	
+	var color: UIColor {
+		switch self {
+		case .success:
+			return #colorLiteral(red: 0.2539775372, green: 0.7368414402, blue: 0.4615401626, alpha: 1)
+		case .error:
+			return #colorLiteral(red: 0.8, green: 0.2, blue: 0.2, alpha: 1)
+		case .normal:
+			return #colorLiteral(red: 0.3607843137, green: 0.3607843137, blue: 0.3607843137, alpha: 1)
+		}
+	}
+}
---
+++ memorize.ai/Modals/NotificationViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class NotificationViewController: UIViewController {
+	@IBOutlet weak var notificationView: UIView!
+	@IBOutlet weak var notificationLabel: UILabel!
+	@IBOutlet weak var notificationLabelBottomConstraint: NSLayoutConstraint!
+	
+	func show(_ delay: TimeInterval, completion: (() -> Void)?) {
+		notificationLabelBottomConstraint.constant = view.safeAreaInsets.bottom + 15
+		view.layoutIfNeeded()
+		notificationView.transform = CGAffineTransform(translationX: 0, y: notificationView.bounds.height)
+		UIView.animate(withDuration: 0.125, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseOut, animations: {
+			self.notificationView.transform = .identity
+		}) {
+			guard $0 else { return }
+			Timer.scheduledTimer(withTimeInterval: delay, repeats: false) { _ in
+				UIView.animate(withDuration: 0.125, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseIn, animations: {
+					self.notificationView.transform = CGAffineTransform(translationX: 0, y: self.notificationView.bounds.height)
+				}) {
+					guard $0 else { return }
+					self.view.removeFromSuperview()
+					completion?()
+				}
+			}
+		}
+	}
+}
