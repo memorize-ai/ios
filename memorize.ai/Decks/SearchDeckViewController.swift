7fec9a4a8df24d845c87a51824bd7f956e359aca
Ken Mueller <kenmueller0@gmail.com>
2019-06-15 03:23:46 -0700

    loads webviews

--- memorize.ai/Decks/SearchDeckViewController.swift
+++ memorize.ai/Decks/SearchDeckViewController.swift
@@ -12,13 +12,15 @@ class SearchDeckViewController: UIViewController, UISearchBarDelegate, UICollect
 		let name: String
 		let subtitle: String
 		let ratings: DeckRatings
+		let deck: Deck?
 		
-		init(id: String, image: UIImage?, name: String, subtitle: String, ratings: DeckRatings) {
+		init(id: String, image: UIImage?, name: String, subtitle: String, ratings: DeckRatings, deck: Deck?) {
 			self.id = id
 			self.image = image
 			self.name = name
 			self.subtitle = subtitle
 			self.ratings = ratings
+			self.deck = deck
 		}
 	}
 	
@@ -57,22 +59,25 @@ class SearchDeckViewController: UIViewController, UISearchBarDelegate, UICollect
 	
 	func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
 		searchResults.removeAll()
-		decksCollectionView.reloadData()
-		if !searchText.trim().isEmpty {
+		if searchText.trim().isEmpty {
+			decksCollectionView.reloadData()
+		} else {
 			Algolia.search(.decks, for: searchText) { results, error in
 				if error == nil {
-					results.forEach { result in
-						let deckId = result["objectID"] as? String ?? "Error"
+					for result in results {
+						guard let deckId = result["objectID"] as? String, !(self.searchResults.contains { $0.id == deckId }) else { continue }
 						listeners["decks/\(deckId)"] = firestore.document("decks/\(deckId)").addSnapshotListener { _, error in
 							guard error == nil, let owner = result["owner"] as? String else { return }
 							listeners["users/\(owner)"] = firestore.document("users/\(owner)").addSnapshotListener { snapshot, userError in
 								guard userError == nil, let snapshot = snapshot else { return }
+								let deck = Deck.get(deckId)
 								self.searchResults.append(SearchResult(
 									id: deckId,
-									image: Deck.get(deckId)?.image,
+									image: deck?.image,
 									name: result["name"] as? String ?? "Error",
 									subtitle: snapshot.get("subtitle") as? String ?? "Error",
-									ratings: DeckRatings(snapshot)
+									ratings: DeckRatings(snapshot),
+									deck: deck
 								))
 								self.decksCollectionView.reloadData()
 							}
@@ -100,14 +105,27 @@ class SearchDeckViewController: UIViewController, UISearchBarDelegate, UICollect
 				guard error == nil, let data = data, let image = UIImage(data: data) else { return }
 				cell.imageView.image = image
 				searchResult.image = image
-				Deck.get(searchResult.id)?.image = image
+				searchResult.deck?.image = image
+				self.decksCollectionView.reloadData()
 			}
 		}
 		cell.nameLabel.text = searchResult.name
 		cell.subtitleLabel.text = searchResult.subtitle
 		cell.setAverageRating(searchResult.ratings.average)
 		cell.ratingsCountLabel.text = String(searchResult.ratings.count)
-		// Load card webviews as well with paginator
+		if let deck = searchResult.deck {
+			let cards = deck.cards.sorted { $0.likes > $1.likes }.prefix(3)
+			for i in 0..<cards.count {
+				cell.load(cell.webViews[i], text: cards[i].front)
+			}
+		} else {
+			firestore.collection("decks/\(searchResult.id)/cards").order(by: "likes").limit(to: 3).getDocuments { snapshot, error in
+				guard error == nil, let snapshot = snapshot?.documents else { return }
+				for i in 0..<snapshot.count {
+					cell.load(cell.webViews[i], text: snapshot[i].get("front") as? String ?? "")
+				}
+			}
+		}
 		return cell
 	}
 	
@@ -127,8 +145,16 @@ class SearchResultCollectionViewCell: UICollectionViewCell {
 	@IBOutlet weak var card2WebView: WKWebView!
 	@IBOutlet weak var card3WebView: WKWebView!
 	
+	var webViews: [WKWebView] {
+		return [card1WebView, card2WebView, card3WebView]
+	}
+	
 	func setAverageRating(_ rating: Double) {
 		starsSliderViewTrailingConstraint.constant = starsSliderView.bounds.width * (rating == 0 ? 1 : CGFloat(5 - rating) / 5)
 		layoutIfNeeded()
+	}
+	
+	func load(_ webView: WKWebView, text: String) {
+		webView.render(text, fontSize: 50, textColor: "000000", backgroundColor: "ffffff")
 	}
 }
