a03c629e658f6b345ffb340a2e962f1ef041bf19
Ken Mueller <kenmueller0@gmail.com>
2019-06-26 09:59:05 +0300

    DEFAULT_PROFILE_PICTURE

--- memorize.ai/Constants.swift
+++ memorize.ai/Constants.swift
@@ -5,3 +5,4 @@ let MAX_FILE_SIZE: Int64 = 50 * 1024 * 1024
 let COMPRESSION_QUALITY: CGFloat = 0.5
 let JPEG_METADATA = StorageMetadata.from(mime: "image/jpeg")
 let DEFAULT_E = 2.5
+let DEFAULT_PROFILE_PICTURE = #imageLiteral(resourceName: "Person")
