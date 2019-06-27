69c1447704bd8680bf362e00040f9007bf00c817
Ken Mueller <kenmueller0@gmail.com>
2019-06-27 21:33:56 +0100

    initialized fields in viewDidLoad

--- memorize.ai/Marketplace/RatingViewController.swift
+++ memorize.ai/Marketplace/RatingViewController.swift
@@ -15,6 +15,16 @@ class RatingViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		guard let rating = rating else { return }
+		userImageView.image = rating.user.image
+		userNameLabel.text = rating.user.name
+		ratingTitleLabel.text = rating.rating.title
+		ratingStarsSliderViewTrailingConstraint.constant = getStarsTrailingConstraint(width: ratingStarsSliderView.bounds.width, rating: Double(rating.rating.rating))
+		ratingDateLabel.text = rating.rating.date.formatCompact()
+		ratingNameLabel.text = rating.user.name
+		let hasReview = rating.rating.hasReview
+		ratingReviewLabel.text = hasReview ? rating.rating.review : "(no review)"
+		ratingReviewLabel.textColor = hasReview ? .black : .darkGray
 	}
 	
 	@IBAction
