0e602ada18bac3789b5ab98cf700c02f42a848a9
Ken Mueller <kenmueller0@gmail.com>
2019-07-08 20:20:03 -0700

    CURRENT_DEVICE

--- memorize.ai/Constants.swift
+++ memorize.ai/Constants.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Firebase
+import DeviceKit
 
 let MAX_FILE_SIZE: Int64 = 50 * 1024 * 1024
 let COMPRESSION_QUALITY: CGFloat = 0.5
@@ -12,3 +13,4 @@ let DEFAULT_BLUE_COLOR = #colorLiteral(red: 0, green: 0.5694751143, blue: 1, alp
 let DEFAULT_RED_COLOR = #colorLiteral(red: 0.8459790349, green: 0.2873021364, blue: 0.2579272389, alpha: 1)
 let CARD_POLL_INTERVAL: TimeInterval = 1
 let PLACEHOLDER_DATE = Date()
+let CURRENT_DEVICE = Device.realDevice(from: Device.current)
