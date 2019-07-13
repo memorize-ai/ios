c2586d39beb88dd3de1c9943af2d2da20f776699
Ken Mueller <kenmueller0@gmail.com>
2019-07-13 09:41:00 -0700

    connected outlets and actions

--- memorize.ai/Games/EmojiGameViewController.swift
+++ memorize.ai/Games/EmojiGameViewController.swift
@@ -1,7 +1,19 @@
 import UIKit
 
 class EmojiGameViewController: UIViewController {
+	@IBOutlet weak var emojiLabel: UILabel!
+	@IBOutlet weak var mainLabel: UILabel!
+	@IBOutlet weak var blockView: UIView!
+	@IBOutlet weak var countTextField: UITextField!
+	@IBOutlet weak var countBarView: UIView!
+	@IBOutlet weak var changeGameStateButton: UIButton!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+	}
+	
+	@IBAction
+	func changeGameState() {
+		
 	}
 }
