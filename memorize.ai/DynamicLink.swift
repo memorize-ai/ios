ff3b671c267a869e52908bdfba78f51019327dd4
Ken Mueller <kenmueller0@gmail.com>
2019-07-17 00:40:23 -0700

    createDynamicLink fixed

--- memorize.ai/DynamicLink.swift
+++ memorize.ai/DynamicLink.swift
@@ -1,12 +1,7 @@
 import Foundation
 
-protocol Linkable {
-	var dynamicLink: URL? { get }
-	var link: URL? { get }
-}
-
-func createDynamicLink(_ ext: String? = nil) -> URL? {
-	return URL(string: "\(DYNAMIC_LINK_BASE_URL)\(ext == nil ? "" : "/\(ext ?? "")")")
+func createDynamicLink(_ ext: String? = nil, title: String, description: String, imageURL imageUrl: String, minimumVersion: String = "1.0") -> URL? {
+	return URL(string: "\(DYNAMIC_LINK_BASE_URL)/?link=\(MEMORIZE_AI_BASE_URL)\(ext == nil ? "" : "/\(ext ?? "")")&ibi=ai.memorize.memorize-ai&ipbi=ai.memorize.memorize-ai&isi=1462251805&imv=\(minimumVersion)&st=\(title)&sd=\(description)&si=\(imageUrl)")
 }
 
 func createLink(_ ext: String? = nil) -> URL? {
