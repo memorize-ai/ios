0f4b925b17d9663115c18fef483d341ea3bcb9d5
Ken Mueller <kenmueller0@gmail.com>
2019-07-17 01:29:24 -0700

    removed linkable from card

--- memorize.ai/Card.swift
+++ memorize.ai/Card.swift
@@ -1,10 +1,8 @@
 import Foundation
 import Firebase
 
-class Card: Linkable {
+class Card {
 	let id: String
-	let dynamicLink: URL?
-	let link: URL?
 	var front: String
 	var back: String
 	let created: Date
@@ -39,9 +37,6 @@ class Card: Linkable {
 		self.next = next
 		self.history = history
 		self.deck = deck
-		let ext = "d/\(deck)/c/\(id)"
-		dynamicLink = createDynamicLink(ext)
-		link = createLink(ext)
 	}
 	
 	convenience init(id: String, front: String, back: String, created: Date, updated: Date, likes: Int, dislikes: Int, deck: String) {
