005c8283939e58738a3eb18de77f8218f326089e
Ken Mueller <kenmueller0@gmail.com>
2019-07-18 21:51:53 -0700

    delete bio

--- memorize.ai/Home/EditBioViewController.swift
+++ memorize.ai/Home/EditBioViewController.swift
@@ -6,7 +6,15 @@ class EditBioViewController: UIViewController, UITextViewDelegate {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		navigationItem.setRightBarButton(UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(saveBio)), animated: true)
+		let deleteBioButton = UIButton(type: .custom)
+		deleteBioButton.setImage(#imageLiteral(resourceName: "Trash Can"), for: .normal)
+		deleteBioButton.addTarget(self, action: #selector(deleteBio), for: .touchUpInside)
+		deleteBioButton.widthAnchor.constraint(equalToConstant: 26).isActive = true
+		deleteBioButton.heightAnchor.constraint(equalToConstant: 26).isActive = true
+		navigationItem.setRightBarButtonItems([
+			UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(saveBio)),
+			UIBarButtonItem(customView: deleteBioButton)
+		], animated: true)
 		bioTextView.setKeyboard(.plain)
 		loadBio()
 	}
@@ -29,12 +37,32 @@ class EditBioViewController: UIViewController, UITextViewDelegate {
 		firestore.document("users/\(id)").updateData(["bio": trimmedText]) { error in
 			guard error == nil else {
 				self.setSaveButtonEnabled(true)
-				self.showNotification("Unable to save. Please try again", type: .error)
+				self.showNotification("Unable to save bio. Please try again", type: .error)
 				return
 			}
 			self.bioTextView.text = trimmedText
 			self.showNotification("Saved", type: .success)
 		}
+	}
+	
+	@objc
+	func deleteBio() {
+		let alertController = UIAlertController(title: "Are you sure?", message: "Your bio will be deleted", preferredStyle: .alert)
+		alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+		alertController.addAction(UIAlertAction(title: "Delete", style: .destructive) { _ in
+			guard let id = id else { return }
+			self.setDeleteButtonEnabled(false)
+			firestore.document("users/\(id)").updateData(["bio": ""]) { error in
+				guard error == nil else {
+					self.setDeleteButtonEnabled(true)
+					self.showNotification("Unable to delete bio. Please try again", type: .error)
+					return
+				}
+				self.bioTextView.text = ""
+				self.navigationController?.popViewController(animated: true)
+			}
+		})
+		present(alertController, animated: true)
 	}
 	
 	func keyboardUp() {
@@ -51,15 +79,23 @@ class EditBioViewController: UIViewController, UITextViewDelegate {
 		guard let bio = bio else { return }
 		bioTextView.text = bio
 		setSaveButtonEnabled(false)
+		setDeleteButtonEnabled(!bio.isEmpty)
 	}
 	
 	func textViewDidChange(_ textView: UITextView) {
-		guard let bio = bio else { return }
-		setSaveButtonEnabled(bioTextView.text != bio)
+		guard let bio = bio, let text = bioTextView.text else { return }
+		setSaveButtonEnabled(text != bio)
+		setDeleteButtonEnabled(!text.isEmpty)
 	}
 	
 	func setSaveButtonEnabled(_ enabled: Bool) {
-		guard let barButton = navigationItem.rightBarButtonItem else { return }
+		guard let barButton = navigationItem.rightBarButtonItems?.first else { return }
+		barButton.isEnabled = enabled
+		barButton.tintColor = enabled ? .white : #colorLiteral(red: 0.9841352105, green: 0.9841352105, blue: 0.9841352105, alpha: 1)
+	}
+	
+	func setDeleteButtonEnabled(_ enabled: Bool) {
+		guard let barButton = navigationItem.rightBarButtonItems?.last else { return }
 		barButton.isEnabled = enabled
 		barButton.tintColor = enabled ? .white : #colorLiteral(red: 0.9841352105, green: 0.9841352105, blue: 0.9841352105, alpha: 1)
 	}
