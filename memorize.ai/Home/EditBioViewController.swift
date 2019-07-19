8f25bec4b00bb0b68fb9e5d6bb2c4926519d7408
Ken Mueller <kenmueller0@gmail.com>
2019-07-18 20:38:13 -0700

    created edit bio vc

---
+++ memorize.ai/Home/EditBioViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class EditBioViewController: UIViewController {
+	@IBOutlet weak var bioTextView: UITextView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		navigationItem.setRightBarButton(<#T##item: UIBarButtonItem?##UIBarButtonItem?#>, animated: <#T##Bool#>)
+	}
+}
