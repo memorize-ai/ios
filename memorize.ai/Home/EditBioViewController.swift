e7bbbdeebaa96fa2e02febd52373fed3e994ceb2
Ken Mueller <kenmueller0@gmail.com>
2019-07-18 20:43:52 -0700

    working on edit bio vc

--- memorize.ai/Home/EditBioViewController.swift
+++ memorize.ai/Home/EditBioViewController.swift
@@ -1,10 +1,23 @@
 import UIKit
 
-class EditBioViewController: UIViewController {
+class EditBioViewController: UIViewController, UITextViewDelegate {
 	@IBOutlet weak var bioTextView: UITextView!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		navigationItem.setRightBarButton(<#T##item: UIBarButtonItem?##UIBarButtonItem?#>, animated: <#T##Bool#>)
+		navigationItem.setRightBarButton(UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(saveBio)), animated: true)
+	}
+	
+	@objc
+	func saveBio() {
+		guard let id = id else { return }
+		firestore.document("users/\(id)").updateData(["bio": bioTextView.text.trim()]) { error in
+			guard error == nil else { return self.showNotification("Unable to save. Please try again", type: .error) }
+			self.showNotification("Saved", type: .success)
+		}
+	}
+	
+	func textViewDidChange(_ textView: UITextView) {
+		<#code#>
 	}
 }
