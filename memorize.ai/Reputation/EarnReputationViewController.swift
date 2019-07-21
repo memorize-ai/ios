db10d11065c02d7db90c4cbd43a47ed6ffe5997f
Ken Mueller <kenmueller0@gmail.com>
2019-07-20 17:04:00 -0700

    setup for earn reputation vc

--- memorize.ai/Reputation/EarnReputationViewController.swift
+++ memorize.ai/Reputation/EarnReputationViewController.swift
@@ -1,7 +1,29 @@
 import UIKit
 
+fileprivate var shouldLoadReputationValues = true
+
 class EarnReputationViewController: UIViewController {
+	@IBOutlet weak var reputationTableView: UITableView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		if shouldLoadReputationValues {
+			shouldLoadReputationValues = false
+			ReputationValue.loadAll()
+		}
+		reputationTableView.tableFooterView = UIView()
 	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		ChangeHandler.update { change in
+			if change == .reputationValueAdded || change == .reputationValueModified || change == .reputationValueRemoved {
+				self.reputationTableView.reloadData()
+			}
+		}
+		updateCurrentViewController()
+	}
+}
+
+class EarnReputationTableViewCell: UITableViewCell {
 }
