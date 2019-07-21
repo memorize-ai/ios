c30c64224c6617d42fc9dfa63658bc931759b59f
Ken Mueller <kenmueller0@gmail.com>
2019-07-20 17:01:17 -0700

    helper functions for reputation value

--- memorize.ai/ReputationValue.swift
+++ memorize.ai/ReputationValue.swift
@@ -1,5 +1,7 @@
 import Firebase
 
+var reputationValues = [ReputationValue]()
+
 class ReputationValue {
 	let id: String
 	var amount: Int
@@ -17,5 +19,56 @@ class ReputationValue {
 			amount: snapshot.get("amount") as? Int ?? 0,
 			order: snapshot.get("order") as? Int ?? 0
 		)
+	}
+	
+	static func loadAll() {
+		firestore.collection("reputation").addSnapshotListener { snapshot, error in
+			guard error == nil, let snapshot = snapshot?.documentChanges else { return }
+			snapshot.forEach {
+				let document = $0.document
+				let documentId = document.documentID
+				switch $0.type {
+				case .added:
+					ReputationValue(document).addToReputationValues()
+					ChangeHandler.call(.reputationValueAdded)
+				case .modified:
+					get(documentId)?.update(document)
+					ChangeHandler.call(.reputationValueModified)
+				case .removed:
+					remove(documentId)
+					ChangeHandler.call(.reputationValueRemoved)
+				@unknown default:
+					break
+				}
+			}
+		}
+	}
+	
+	static func get(_ id: String) -> ReputationValue? {
+		return reputationValues.first { $0.id == id }
+	}
+	
+	static func remove(_ id: String) {
+		reputationValues = reputationValues.filter { $0.id != id }
+		sortReputationValues()
+	}
+	
+	private static func sortReputationValues() {
+		reputationValues.sort { $0.order < $1.order }
+	}
+	
+	@discardableResult
+	func addToReputationValues() -> ReputationValue {
+		reputationValues.append(self)
+		ReputationValue.sortReputationValues()
+		return self
+	}
+	
+	@discardableResult
+	func update(_ snapshot: DocumentSnapshot) -> ReputationValue {
+		amount = snapshot.get("amount") as? Int ?? amount
+		order = snapshot.get("order") as? Int ?? order
+		ReputationValue.sortReputationValues()
+		return self
 	}
 }
