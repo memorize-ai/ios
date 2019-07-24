86338f19d18e04611fd38504f0f4008cf8004f44
Ken Mueller <kenmueller0@gmail.com>
2019-07-24 01:46:16 -0700

    user vc show method

--- memorize.ai/User/UserViewController.swift
+++ memorize.ai/User/UserViewController.swift
@@ -1,7 +1,17 @@
 import UIKit
 
 class UserViewController: UIViewController {
+	var uid: String?
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+	}
+	
+	@discardableResult
+	static func show(_ viewController: UIViewController, id uid: String) -> Bool {
+		guard let userVC = viewController.storyboard?.instantiateViewController(withIdentifier: "user") as? UserViewController else { return false }
+		userVC.uid = uid
+		viewController.navigationController?.pushViewController(userVC, animated: true)
+		return true
 	}
 }
