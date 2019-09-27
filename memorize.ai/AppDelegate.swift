ec475e0f206073235d4c415180edd87481e53961
Ken Mueller <kenmueller0@gmail.com>
2019-09-27 15:20:20 -0700

    added dev build profile

--- memorize.ai/AppDelegate.swift
+++ memorize.ai/AppDelegate.swift
@@ -1,10 +1,12 @@
 import UIKit
 import CoreData
+import FirebaseCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		true
+		FirebaseApp.configure()
+		return true
 	}
 	
 	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
---
+++ memorize.ai/Global.swift
@@ -0,0 +1,7 @@
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseStorage
+
+let auth = Auth.auth()
+let firestore = Firestore.firestore()
+let storage = Storage.storage().reference()
