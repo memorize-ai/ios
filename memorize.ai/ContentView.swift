6076c52aeb257c7ec67019a9f3577541a9f63fd2
Ken Mueller <kenmueller0@gmail.com>
2019-09-27 00:12:32 -0700

    Initial commit

---
+++ memorize.ai/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  memorize.ai
+//
+//  Created by Ken Mueller on 9/26/19.
+//  Copyright © 2019 Ken Mueller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
