62978a9a1ef15cb123c123e42ce3dc112173f9e6
Ken Mueller <kenmueller0@gmail.com>
2019-09-28 00:35:13 -0700

    extracted deckstore cases into methods

--- memorize.ai/Classes/DeckStore.swift
+++ memorize.ai/Classes/DeckStore.swift
@@ -1,4 +1,5 @@
 import Combine
+import FirebaseFirestore
 
 final class DeckStore: ObservableObject {
 	@Published var decks: [Deck]
@@ -14,22 +15,13 @@ final class DeckStore: ObservableObject {
 		loadingState = .loading()
 		firestore.collection("users/\(user.id)/decks").addSnapshotListener().done { snapshot in
 			snapshot.documentChanges.forEach { documentChange in
-				let document = documentChange.document
-				let deckId = document.documentID
+				let userDocument = documentChange.document
+				let deckId = userDocument.documentID
 				switch documentChange.type {
 				case .added:
-					let deck = Deck(id: deckId)
-					deck.publicDocument.addSnapshotListener().done { publicDeckSnapshot in
-						self.objectWillChange.send()
-						deck.updatePublicData(document: publicDeckSnapshot)
-					}.catch { error in
-						self.loadingState = .failure(message: error.localizedDescription)
-					}
-					self.decks.append(deck)
+					self.handleDeckAdded(id: deckId)
 				case .modified:
-					guard let deck = self.get(id: deckId) else { return }
-					self.objectWillChange.send()
-					deck.updateUserData(document: document)
+					self.handleDeckModified(id: deckId, userDocument: userDocument)
 				case .removed:
 					self.decks = self.decks.filter { $0.id != deckId }
 				@unknown default:
@@ -41,6 +33,23 @@ final class DeckStore: ObservableObject {
 			self.loadingState = .failure(message: error.localizedDescription)
 		}
 		return self
+	}
+	
+	private func handleDeckAdded(id: String) {
+		let deck = Deck(id: id)
+		decks.append(deck)
+		deck.publicDocument.addSnapshotListener().done { publicDeckSnapshot in
+			self.objectWillChange.send()
+			deck.updatePublicData(document: publicDeckSnapshot)
+		}.catch { error in
+			self.loadingState = .failure(message: error.localizedDescription)
+		}
+	}
+	
+	private func handleDeckModified(id: String, userDocument: DocumentSnapshot) {
+		guard let deck = get(id: id) else { return }
+		objectWillChange.send()
+		deck.updateUserData(document: userDocument)
 	}
 	
 	func get(id: String) -> Deck? {
