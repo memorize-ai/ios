0bc6311c221341b5b20ee793b5334001eee8a301
Ken Mueller <kenmueller0@gmail.com>
2019-09-28 15:44:40 -0700

    added SignInView.swift

---
+++ memorize.ai/Views/Authentication/Sign In/SignInView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct SignInView: View {
+	var body: some View {
+		Text("SignInView")
+	}
+}
+
+#if DEBUG
+struct SignInView_Previews: PreviewProvider {
+	static var previews: some View {
+		SignInView()
+	}
+}
+#endif
