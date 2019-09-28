c89e20306630643bcd55509aa564c8fd39c26d2e
Ken Mueller <kenmueller0@gmail.com>
2019-09-28 16:46:15 -0700

    removed ui tests

--- memorize.aiUITests/memorize_aiUITests.swift
+++ memorize.aiUITests/memorize_aiUITests.swift
@@ -1,10 +1,3 @@
 import XCTest
 
-final class memorize_aiUITests: XCTestCase {
-	func testLaunchPerformance() {
-		guard #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) else { return }
-		measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-			XCUIApplication().launch()
-		}
-	}
-}
+final class memorize_aiUITests: XCTestCase {}
