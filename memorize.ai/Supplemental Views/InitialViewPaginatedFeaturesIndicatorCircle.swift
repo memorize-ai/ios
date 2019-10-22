a76194f0d3ec383c2c44b1899103d93eec759d52
Ken Mueller <kenmueller0@gmail.com>
2019-10-22 12:01:33 -0700

    worked on InitialViewPaginatedFeaturesIndicatorCircle

--- memorize.ai/Supplemental Views/InitialViewPaginatedFeaturesIndicatorCircle.swift
+++ memorize.ai/Supplemental Views/InitialViewPaginatedFeaturesIndicatorCircle.swift
@@ -1,10 +1,14 @@
 import SwiftUI
 
 struct InitialViewPaginatedFeaturesIndicatorCircle: View {
+	static let width: CGFloat = 10
+	
 	let active: Bool
 	
 	var body: some View {
-		Text("InitialViewPaginatedFeaturesIndicatorCircle")
+		Circle()
+			.frame(width: Self.width)
+			.foregroundColor(Color.darkBlue.opacity(active ? 1 : 0.2))
 	}
 }
 
