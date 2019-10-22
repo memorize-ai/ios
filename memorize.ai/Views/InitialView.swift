af7d17ceeb84eab7fc4757c12fda80b3019f806d
Ken Mueller <kenmueller0@gmail.com>
2019-10-21 18:02:32 -0700

    testing initialview

--- memorize.ai/Views/InitialView.swift
+++ memorize.ai/Views/InitialView.swift
@@ -2,7 +2,31 @@ import SwiftUI
 
 struct InitialView: View {
 	var body: some View {
-		Text("InitialView")
+		VStack {
+			Spacer()
+			HStack(spacing: 16) {
+				CustomRectangle {
+					Text("SIGN UP")
+						.frame(maxWidth: 200, maxHeight: 40)
+						.font(.muli(.bold, size: 14))
+						.foregroundColor(.darkBlue)
+				}
+				CustomRectangle(
+					backgroundColor: .transparent,
+					borderColor: Color.gray.opacity(0.5),
+					borderWidth: 1.5
+				) {
+					Text("LOGIN")
+						.frame(maxWidth: 200, maxHeight: 40)
+						.font(.muli(.bold, size: 14))
+						.foregroundColor(.white)
+				}
+			}
+			.padding(.horizontal, 48)
+			.padding(.bottom, 29.64)
+			.frame(maxWidth: .infinity)
+			.background(Color.green)
+		}
 	}
 }
 
