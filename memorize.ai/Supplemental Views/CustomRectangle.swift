1fa54eb35fe8a525db8574f5d30a30264075aebd
Ken Mueller <kenmueller0@gmail.com>
2019-10-21 18:02:28 -0700

    renamed to CustomRectangle

--- memorize.ai/Supplemental Views/CustomRectangle.swift
+++ memorize.ai/Supplemental Views/CustomRectangle.swift
@@ -1,11 +1,10 @@
 import SwiftUI
 
-struct RectangleButton<Content: View>: View {
+struct CustomRectangle<Content: View>: View {
 	let backgroundColor: Color
 	let borderColor: Color
 	let borderWidth: CGFloat
 	let cornerRadius: CGFloat
-	let action: () -> Void
 	let content: () -> Content
 	
 	init(
@@ -13,44 +12,40 @@ struct RectangleButton<Content: View>: View {
 		borderColor: Color = .white,
 		borderWidth: CGFloat = 0,
 		cornerRadius: CGFloat = 5,
-		action: @escaping () -> Void,
 		content: @escaping () -> Content
 	) {
 		self.backgroundColor = backgroundColor
 		self.borderColor = borderColor
 		self.borderWidth = borderWidth
 		self.cornerRadius = cornerRadius
-		self.action = action
 		self.content = content
 	}
 	
 	var body: some View {
 		let roundedRectangle = RoundedRectangle(cornerRadius: cornerRadius)
-		return Button(action: action) {
-			content()
-				.background(
-					backgroundColor
-						.clipShape(roundedRectangle)
-				)
-				.overlay(
-					roundedRectangle
-						.stroke(borderColor, lineWidth: borderWidth)
-				)
-		}
+		return content()
+			.background(
+				backgroundColor
+					.clipShape(roundedRectangle)
+			)
+			.overlay(
+				roundedRectangle
+					.stroke(borderColor, lineWidth: borderWidth)
+			)
 	}
 }
 
 #if DEBUG
-struct RectangleButton_Previews: PreviewProvider {
+struct CustomRectangle_Previews: PreviewProvider {
 	static var previews: some View {
 		VStack(spacing: 20) {
-			RectangleButton(action: {}) {
+			CustomRectangle {
 				Text("SIGN UP")
 					.frame(maxWidth: 132, maxHeight: 40)
 					.font(.muli(.bold, size: 14))
 					.foregroundColor(.darkBlue)
 			}
-			RectangleButton(borderColor: .darkBlue, borderWidth: 3, action: {}) {
+			CustomRectangle(borderColor: .darkBlue, borderWidth: 3) {
 				Text("SIGN UP")
 					.frame(maxWidth: 132, maxHeight: 40)
 					.font(.muli(.bold, size: 14))
