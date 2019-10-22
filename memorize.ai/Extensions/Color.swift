c6c9034402c1e9c40932842a391a7253c947758d
Ken Mueller <kenmueller0@gmail.com>
2019-10-21 21:41:14 -0700

    InitialViewBottomGradient

--- memorize.ai/Extensions/Color.swift
+++ memorize.ai/Extensions/Color.swift
@@ -2,5 +2,7 @@ import SwiftUI
 
 extension Color {
 	static let transparent = Self.white.opacity(0)
+	static let lightBlue = Self("LightBlue")
+	static let mediumBlue = Self("MediumBlue")
 	static let darkBlue = Self("DarkBlue")
 }
---
+++ memorize.ai/Supplemental Views/InitialViewBottomGradient.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct InitialViewBottomGradient: View {
+	static let height: CGFloat = 171.64
+	static let midHeight: CGFloat = 91.64
+	
+	var body: some View {
+		GeometryReader { geometry in
+			Path { path in
+				let width = geometry.size.width
+				path.addLines([
+					.init(x: 0, y: Self.height),
+					.init(x: 0, y: Self.height - Self.midHeight),
+					.init(x: width, y: 0),
+					.init(x: width, y: Self.height)
+				])
+			}
+			.fill(LinearGradient(
+				gradient: .init(colors: [.lightBlue, .mediumBlue]),
+				startPoint: .init(x: 0.5, y: 0),
+				endPoint: .init(x: 0.5, y: 1)
+			))
+			.frame(width: geometry.size.width, height: 171.64)
+		}
+	}
+}
+
+#if DEBUG
+struct InitialViewBottomGradient_Previews: PreviewProvider {
+	static var previews: some View {
+		InitialViewBottomGradient()
+	}
+}
+#endif
