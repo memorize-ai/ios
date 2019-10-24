0782b1b2f3786aa610131002be4151f5938bc94f
Ken Mueller <kenmueller0@gmail.com>
2019-10-24 12:42:26 -0700

    fixed custom text field

--- memorize.ai/Supplemental Views/CustomTextField.swift
+++ memorize.ai/Supplemental Views/CustomTextField.swift
@@ -1,29 +1,42 @@
 import SwiftUI
 
 struct CustomTextField: View {
+	static let defaultBackgroundColor = Color.mediumGray.opacity(0.6799)
+	
 	@Binding var text: String
 	
 	let placeholder: String
+	let internalPadding: CGFloat
 	let textColor: Color
 	let font: Font
+	let backgroundColor: Color
+	let cornerRadius: CGFloat
 	
 	init(
 		_ text: Binding<String>,
 		placeholder: String = "",
+		internalPadding: CGFloat = 10,
 		textColor: Color = .darkText,
-		font: Font = .muli(.regular, size: 14)
+		font: Font = .muli(.regular, size: 14),
+		backgroundColor: Color = Self.defaultBackgroundColor,
+		cornerRadius: CGFloat = 5
 	) {
 		_text = text
 		self.placeholder = placeholder
+		self.internalPadding = internalPadding
 		self.textColor = textColor
 		self.font = font
+		self.backgroundColor = backgroundColor
+		self.cornerRadius = cornerRadius
 	}
 	
 	var body: some View {
 		TextField(placeholder, text: $text)
-			.textFieldStyle(RoundedBorderTextFieldStyle())
+			.padding(internalPadding)
+			.background(backgroundColor)
 			.foregroundColor(textColor)
 			.font(font)
+			.cornerRadius(cornerRadius)
 	}
 }
 
