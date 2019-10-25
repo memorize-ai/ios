29b77638a364f21f67887361006a67dd2aa5d208
Ken Mueller <kenmueller0@gmail.com>
2019-10-25 00:15:27 -0700

    NavigateTo

---
+++ memorize.ai/Supplemental Views/NavigateTo.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+struct NavigateTo<Destination: View>: View {
+	let destination: Destination
+	
+	@Binding var isActive: Bool
+	
+	init(
+		_ destination: Destination,
+		when isActive: Binding<Bool>
+	) {
+		self.destination = destination
+		_isActive = isActive
+	}
+	
+	var body: some View {
+		NavigationLink(
+			destination: destination,
+			isActive: $isActive
+		) {
+			EmptyView()
+		}
+		.hidden()
+	}
+}
+
+#if DEBUG
+struct NavigateTo_Previews: PreviewProvider {
+	static var previews: some View {
+		NavigateTo(Text("Hello!"), when: .constant(false))
+	}
+}
+#endif
