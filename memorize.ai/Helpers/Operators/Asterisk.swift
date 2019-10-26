1c617dce37514760eb15d900f8662194a21606b9
Ken Mueller <kenmueller0@gmail.com>
2019-10-26 01:09:16 -0700

    numeric protocol

--- memorize.ai/Helpers/Operators/Asterisk.swift
+++ memorize.ai/Helpers/Operators/Asterisk.swift
@@ -2,14 +2,6 @@ import CoreGraphics
 
 prefix operator *
 
-prefix func * (_ bool: Bool) -> Int {
-	bool ? 1 : 0
-}
-
-prefix func * (_ bool: Bool) -> Double {
-	bool ? 1 : 0
-}
-
-prefix func * (_ bool: Bool) -> CGFloat {
+prefix func * <T: Numeric>(_ bool: Bool) -> T {
 	bool ? 1 : 0
 }
