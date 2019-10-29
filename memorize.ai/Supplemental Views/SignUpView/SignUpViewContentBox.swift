a280fef33daee8e7d03a73e8d74d5dac81a0fddd
Ken Mueller <kenmueller0@gmail.com>
2019-10-29 01:32:58 -0700

    added or sign in with google

--- memorize.ai/Supplemental Views/SignUpView/SignUpViewContentBox.swift
+++ memorize.ai/Supplemental Views/SignUpView/SignUpViewContentBox.swift
@@ -4,52 +4,56 @@ struct SignUpViewContentBox: View {
 	@ObservedObject var model: SignUpViewModel
 	
 	var body: some View {
-		VStack(spacing: 32) {
-			VStack(spacing: 12) {
-				CustomTextField(
-					$model.name,
-					placeholder: "Name",
-					contentType: .name,
-					keyboardType: .alphabet,
-					capitalization: .words,
-					borderWidth: *model.shouldShowNameRedBorder
-				)
-				CustomTextField(
-					$model.email,
-					placeholder: "Email",
-					contentType: .emailAddress,
-					keyboardType: .emailAddress,
-					capitalization: .none,
-					borderWidth: *model.shouldShowEmailRedBorder
-				)
-				CustomTextField(
-					$model.password,
-					placeholder: "Password",
-					contentType: .newPassword,
-					capitalization: .none,
-					secure: true,
-					borderWidth: *model.shouldShowPasswordRedBorder
-				)
-			}
-			Button(action: model.signUp) {
-				CustomRectangle(
-					backgroundColor: model.isSignUpButtonDisabled
-						? .disabledButtonBackground
-						: .darkBlue
-				) {
-					Group {
-						if model.loadingState.isLoading {
-							ActivityIndicator()
-						} else {
-							Text("SIGN UP")
-								.font(.muli(.bold, size: 14))
-								.foregroundColor(.white)
+		VStack(spacing: 20) {
+			VStack(spacing: 32) {
+				VStack(spacing: 12) {
+					CustomTextField(
+						$model.name,
+						placeholder: "Name",
+						contentType: .name,
+						keyboardType: .alphabet,
+						capitalization: .words,
+						borderWidth: *model.shouldShowNameRedBorder
+					)
+					CustomTextField(
+						$model.email,
+						placeholder: "Email",
+						contentType: .emailAddress,
+						keyboardType: .emailAddress,
+						capitalization: .none,
+						borderWidth: *model.shouldShowEmailRedBorder
+					)
+					CustomTextField(
+						$model.password,
+						placeholder: "Password",
+						contentType: .newPassword,
+						capitalization: .none,
+						secure: true,
+						borderWidth: *model.shouldShowPasswordRedBorder
+					)
+				}
+				Button(action: model.signUp) {
+					CustomRectangle(
+						backgroundColor: model.isSignUpButtonDisabled
+							? .disabledButtonBackground
+							: .darkBlue
+					) {
+						Group {
+							if model.loadingState.isLoading {
+								ActivityIndicator()
+							} else {
+								Text("SIGN UP")
+									.font(.muli(.bold, size: 14))
+									.foregroundColor(.white)
+							}
 						}
+						.frame(maxWidth: .infinity)
+						.frame(height: 40)
 					}
-					.frame(maxWidth: .infinity)
-					.frame(height: 40)
 				}
 			}
+			AuthenticationViewOrText()
+			GoogleSignInButton()
 		}
 	}
 }
