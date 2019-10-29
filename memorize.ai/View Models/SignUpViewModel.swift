fef7587cf146470d01e39ee6643bee0c678116ff
Ken Mueller <kenmueller0@gmail.com>
2019-10-29 10:32:31 -0700

    working on sign up view model

--- memorize.ai/View Models/SignUpViewModel.swift
+++ memorize.ai/View Models/SignUpViewModel.swift
@@ -1,4 +1,6 @@
 import Combine
+import FirebaseAuth
+import PromiseKit
 
 final class SignUpViewModel: ViewModel {
 	@Published var name = ""
@@ -10,6 +12,10 @@ final class SignUpViewModel: ViewModel {
 	@Published var shouldShowPasswordRedBorder = false
 	
 	@Published var loadingState = LoadingState.none
+	@Published var shouldShowErrorModal = false
+	
+	var user: User?
+	var errorModal: (title: String, description: String)?
 	
 	var isSignUpButtonDisabled: Bool {
 		name.isTrimmedEmpty ||
@@ -18,6 +24,46 @@ final class SignUpViewModel: ViewModel {
 	}
 	
 	func signUp() {
-		// TODO: Sign up
+		loadingState = .loading()
+		resetRedBorders()
+		auth.createUser(
+			withEmail: email,
+			password: password
+		).done { result in
+			let uid = result.user.uid
+			self.createUserInFirestore(uid: uid).done {
+				self.setUser(uid: uid)
+				self.loadingState = .success()
+			}.catch(self.failFirestoreSignUp)
+		}.catch(failAuthSignUp)
+	}
+	
+	func setUser(uid: String) {
+		user = .init(
+			id: uid,
+			name: name,
+			email: email
+		)
+	}
+	
+	func createUserInFirestore(uid: String) -> Promise<Void> {
+		firestore.document("users/\(uid)").setData([
+			"name": name,
+			"email": email
+		])
+	}
+	
+	func failAuthSignUp(error: Error) {
+		// TODO: Fail auth sign up
+	}
+	
+	func failFirestoreSignUp(error: Error) {
+		// TODO: Fail firestore sign up
+	}
+	
+	func resetRedBorders() {
+		shouldShowNameRedBorder = false
+		shouldShowEmailRedBorder = false
+		shouldShowPasswordRedBorder = false
 	}
 }
