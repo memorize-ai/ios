e9c7fac6cd3d6192fc24cef1ae572f8d8d6fde81
Ken Mueller <kenmueller0@gmail.com>
2019-10-31 15:14:15 -0700

    deck cell get button works with clicking

--- memorize.ai/Supplemental Views/Deck/DeckCellWithGetButton.swift
+++ memorize.ai/Supplemental Views/Deck/DeckCellWithGetButton.swift
@@ -3,27 +3,32 @@ import SwiftUI
 struct DeckCellWithGetButton: View {
 	let deck: Deck
 	let width: CGFloat
+	let isLoading: Bool
 	let onGetClick: () -> Void
 	
 	var body: some View {
-		DeckCell(
-			deck: deck,
-			width: width
-		) {
+		DeckCell(deck: deck, width: width) {
 			Button(action: onGetClick) {
 				CustomRectangle(
 					background: Color.darkBlue,
 					cornerRadius: 8
 				) {
-					Text("GET")
-						.font(.muli(.bold, size: 12))
-						.foregroundColor(.white)
-						.frame(height: 33)
-						.frame(maxWidth: .infinity)
+					Group {
+						if isLoading {
+							Text("GET")
+								.font(.muli(.bold, size: 12))
+						} else {
+							ActivityIndicator(radius: 8)
+						}
+					}
+					.foregroundColor(.white)
+					.frame(height: 33)
+					.frame(maxWidth: .infinity)
 				}
 				.padding([.horizontal, .bottom], 18)
 				.padding(.top, 10)
 			}
+			.disabled(isLoading)
 		}
 	}
 }
@@ -31,19 +36,36 @@ struct DeckCellWithGetButton: View {
 #if DEBUG
 struct DeckCellWithGetButton_Previews: PreviewProvider {
 	static var previews: some View {
-		DeckCellWithGetButton(
-			deck: .init(
-				id: "0",
-				name: "Geometry Prep",
-				subtitle: "Angles, lines, triangles and other polygons",
-				numberOfViews: 1000000000,
-				numberOfUniqueViews: 200000,
-				numberOfRatings: 12400,
-				averageRating: 4.5,
-				numberOfDownloads: 196400
-			),
-			width: 165
-		) {}
+		VStack(spacing: 20) {
+			DeckCellWithGetButton(
+				deck: .init(
+					id: "0",
+					name: "Geometry Prep",
+					subtitle: "Angles, lines, triangles and other polygons",
+					numberOfViews: 1000000000,
+					numberOfUniqueViews: 200000,
+					numberOfRatings: 12400,
+					averageRating: 4.5,
+					numberOfDownloads: 196400
+				),
+				width: 165,
+				isLoading: false
+			) {}
+			DeckCellWithGetButton(
+				deck: .init(
+					id: "0",
+					name: "Geometry Prep",
+					subtitle: "Angles, lines, triangles and other polygons",
+					numberOfViews: 1000000000,
+					numberOfUniqueViews: 200000,
+					numberOfRatings: 12400,
+					averageRating: 4.5,
+					numberOfDownloads: 196400
+				),
+				width: 165,
+				isLoading: true
+			) {}
+		}
 	}
 }
 #endif
