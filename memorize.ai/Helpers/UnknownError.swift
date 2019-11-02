87f9bbec8f338e23267671fdefb4257d939e195a
Ken Mueller <kenmueller0@gmail.com>
2019-11-02 16:39:31 -0700

    added localized description to unknown error

--- memorize.ai/Helpers/UnknownError.swift
+++ memorize.ai/Helpers/UnknownError.swift
@@ -1,5 +1,11 @@
+import Foundation
+
 let UNKNOWN_ERROR = UnknownError.default
 
-enum UnknownError: Error {
+enum UnknownError: LocalizedError {
 	case `default`
+	
+	var localizedDescription: String {
+		"Unknown error"
+	}
 }
