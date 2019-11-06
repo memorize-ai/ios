260b12d9bb1b266cd53060506714982727858d24
Ken Mueller <kenmueller0@gmail.com>
2019-11-06 11:46:04 -0800

    removed useless extension

--- memorize.ai/Extensions/FirebaseFirestore+PromiseKit.swift
+++ memorize.ai/Extensions/FirebaseFirestore+PromiseKit.swift
@@ -1,19 +1,6 @@
 import PromiseKit
 import FirebaseFirestore
 
-extension CollectionReference {
-	func addSnapshotListener() -> Promise<QuerySnapshot> {
-		.init { seal in
-			addSnapshotListener { snapshot, error in
-				guard error == nil, let snapshot = snapshot else {
-					return seal.reject(error ?? UNKNOWN_ERROR)
-				}
-				seal.fulfill(snapshot)
-			}
-		}
-	}
-}
-
 extension DocumentReference {
 	func setData(_ documentData: [String: Any]) -> Promise<Void> {
 		.init { seal in
