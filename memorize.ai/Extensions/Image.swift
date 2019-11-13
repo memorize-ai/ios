6806c6a5a169dc89072a6b7be8a446a95a9678ec
Ken Mueller <kenmueller0@gmail.com>
2019-11-12 23:30:45 -0800

    added tab bar item images

--- memorize.ai/Extensions/Image.swift
+++ memorize.ai/Extensions/Image.swift
@@ -6,4 +6,10 @@ extension Image {
 	static let grayDownloadIcon = Self("GrayDownloadIcon")
 	static let deckStar = Self("DeckStar")
 	static let grayMagnifyingGlass = Self("GrayMagnifyingGlass")
+	static let homeTabBarItem = Self("HomeTabBarItem")
+	static let selectedHomeTabBarItem = Self("SelectedHomeTabBarItem")
+	static let marketTabBarItem = Self("MarketTabBarItem")
+	static let selectedMarketTabBarItem = Self("SelectedMarketTabBarItem")
+	static let profileTabBarItem = Self("ProfileTabBarItem")
+	static let selectedProfileTabBarItem = Self("SelectedProfileTabBarItem")
 }
