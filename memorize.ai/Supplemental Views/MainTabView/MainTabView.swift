c0b68c6277a9d80932b7791ff74f8c59170c828c
Ken Mueller <kenmueller0@gmail.com>
2019-11-14 00:10:48 -0800

    added previews for topics

--- memorize.ai/Supplemental Views/MainTabView/MainTabView.swift
+++ memorize.ai/Supplemental Views/MainTabView/MainTabView.swift
@@ -140,130 +140,162 @@ struct MainTabView_Previews: PreviewProvider {
 			)
 		)
 		failedDeck.imageLoadingState.fail(message: "Self-invoked")
-		return MainTabView()
-			.environmentObject(CurrentStore(user: .init(
+		let currentStore = CurrentStore(user: .init(
+			id: "0",
+			name: "Ken Mueller",
+			email: "kenmueller0@gmail.com",
+			interests: [
+				"0",
+				"1",
+				"2",
+				"3"
+			],
+			numberOfDecks: 4,
+			decks: [
+				.init(
+					id: "0",
+					topics: [],
+					hasImage: true,
+					image: .init("GeometryPrepDeck"),
+					name: "Geometry Prep #1",
+					subtitle: "Angles, lines, triangles and other polygons",
+					numberOfViews: 1000000000,
+					numberOfUniqueViews: 200000,
+					numberOfRatings: 12400,
+					averageRating: 4.5,
+					numberOfDownloads: 196400,
+					dateCreated: .init(),
+					dateLastUpdated: .init(),
+					userData: .init(
+						dateAdded: .init(),
+						isFavorite: false,
+						numberOfDueCards: 23
+					)
+				),
+				.init(
+					id: "1",
+					topics: [],
+					hasImage: true,
+					image: .init("GeometryPrepDeck"),
+					name: "Geometry Prep #2",
+					subtitle: "Angles, lines, triangles and other polygons",
+					numberOfViews: 1000000000,
+					numberOfUniqueViews: 200000,
+					numberOfRatings: 12400,
+					averageRating: 4.5,
+					numberOfDownloads: 196400,
+					dateCreated: .init(),
+					dateLastUpdated: .init(),
+					userData: .init(
+						dateAdded: .init(),
+						isFavorite: true,
+						numberOfDueCards: 0
+					)
+				),
+				.init(
+					id: "2",
+					topics: [],
+					hasImage: true,
+					image: .init("GeometryPrepDeck"),
+					name: "Geometry Prep #3",
+					subtitle: "Angles, lines, triangles and other polygons",
+					numberOfViews: 1000000000,
+					numberOfUniqueViews: 200000,
+					numberOfRatings: 12400,
+					averageRating: 4.5,
+					numberOfDownloads: 196400,
+					dateCreated: .init(),
+					dateLastUpdated: .init()
+				),
+				.init(
+					id: "3",
+					topics: [],
+					hasImage: true,
+					image: .init("GeometryPrepDeck"),
+					name: "Geometry Prep #4",
+					subtitle: "Angles, lines, triangles and other polygons",
+					numberOfViews: 1000000000,
+					numberOfUniqueViews: 200000,
+					numberOfRatings: 12400,
+					averageRating: 4.5,
+					numberOfDownloads: 196400,
+					dateCreated: .init(),
+					dateLastUpdated: .init(),
+					userData: .init(
+						dateAdded: .init(),
+						isFavorite: true,
+						numberOfDueCards: 36
+					)
+				),
+				.init(
+					id: "4",
+					topics: [],
+					hasImage: false,
+					name: "Geometry Prep #5",
+					subtitle: "Angles, lines, triangles and other polygons",
+					numberOfViews: 1000000000,
+					numberOfUniqueViews: 200000,
+					numberOfRatings: 12400,
+					averageRating: 4.5,
+					numberOfDownloads: 196400,
+					dateCreated: .init(),
+					dateLastUpdated: .init(),
+					userData: .init(
+						dateAdded: .init(),
+						isFavorite: false,
+						numberOfDueCards: 568
+					)
+				),
+				.init(
+					id: "5",
+					topics: [],
+					hasImage: true,
+					name: "Geometry Prep #6",
+					subtitle: "Angles, lines, triangles and other polygons",
+					numberOfViews: 1000000000,
+					numberOfUniqueViews: 200000,
+					numberOfRatings: 12400,
+					averageRating: 4.5,
+					numberOfDownloads: 196400,
+					dateCreated: .init(),
+					dateLastUpdated: .init(),
+					userData: .init(
+						dateAdded: .init(),
+						isFavorite: true,
+						numberOfDueCards: 1
+					)
+				),
+				failedDeck
+			]
+		))
+		currentStore.topics = [
+			.init(
 				id: "0",
-				name: "Ken Mueller",
-				email: "kenmueller0@gmail.com",
-				interests: [],
-				numberOfDecks: 4,
-				decks: [
-					.init(
-						id: "0",
-						topics: [],
-						hasImage: true,
-						image: .init("GeometryPrepDeck"),
-						name: "Geometry Prep #1",
-						subtitle: "Angles, lines, triangles and other polygons",
-						numberOfViews: 1000000000,
-						numberOfUniqueViews: 200000,
-						numberOfRatings: 12400,
-						averageRating: 4.5,
-						numberOfDownloads: 196400,
-						dateCreated: .init(),
-						dateLastUpdated: .init(),
-						userData: .init(
-							dateAdded: .init(),
-							isFavorite: false,
-							numberOfDueCards: 23
-						)
-					),
-					.init(
-						id: "1",
-						topics: [],
-						hasImage: true,
-						image: .init("GeometryPrepDeck"),
-						name: "Geometry Prep #2",
-						subtitle: "Angles, lines, triangles and other polygons",
-						numberOfViews: 1000000000,
-						numberOfUniqueViews: 200000,
-						numberOfRatings: 12400,
-						averageRating: 4.5,
-						numberOfDownloads: 196400,
-						dateCreated: .init(),
-						dateLastUpdated: .init(),
-						userData: .init(
-							dateAdded: .init(),
-							isFavorite: true,
-							numberOfDueCards: 0
-						)
-					),
-					.init(
-						id: "2",
-						topics: [],
-						hasImage: true,
-						image: .init("GeometryPrepDeck"),
-						name: "Geometry Prep #3",
-						subtitle: "Angles, lines, triangles and other polygons",
-						numberOfViews: 1000000000,
-						numberOfUniqueViews: 200000,
-						numberOfRatings: 12400,
-						averageRating: 4.5,
-						numberOfDownloads: 196400,
-						dateCreated: .init(),
-						dateLastUpdated: .init()
-					),
-					.init(
-						id: "3",
-						topics: [],
-						hasImage: true,
-						image: .init("GeometryPrepDeck"),
-						name: "Geometry Prep #4",
-						subtitle: "Angles, lines, triangles and other polygons",
-						numberOfViews: 1000000000,
-						numberOfUniqueViews: 200000,
-						numberOfRatings: 12400,
-						averageRating: 4.5,
-						numberOfDownloads: 196400,
-						dateCreated: .init(),
-						dateLastUpdated: .init(),
-						userData: .init(
-							dateAdded: .init(),
-							isFavorite: true,
-							numberOfDueCards: 36
-						)
-					),
-					.init(
-						id: "4",
-						topics: [],
-						hasImage: false,
-						name: "Geometry Prep #5",
-						subtitle: "Angles, lines, triangles and other polygons",
-						numberOfViews: 1000000000,
-						numberOfUniqueViews: 200000,
-						numberOfRatings: 12400,
-						averageRating: 4.5,
-						numberOfDownloads: 196400,
-						dateCreated: .init(),
-						dateLastUpdated: .init(),
-						userData: .init(
-							dateAdded: .init(),
-							isFavorite: false,
-							numberOfDueCards: 568
-						)
-					),
-					.init(
-						id: "5",
-						topics: [],
-						hasImage: true,
-						name: "Geometry Prep #6",
-						subtitle: "Angles, lines, triangles and other polygons",
-						numberOfViews: 1000000000,
-						numberOfUniqueViews: 200000,
-						numberOfRatings: 12400,
-						averageRating: 4.5,
-						numberOfDownloads: 196400,
-						dateCreated: .init(),
-						dateLastUpdated: .init(),
-						userData: .init(
-							dateAdded: .init(),
-							isFavorite: true,
-							numberOfDueCards: 1
-						)
-					),
-					failedDeck
-				]
-			)))
+				name: "Math",
+				image: .init("GeometryTopic"),
+				topDecks: []
+			),
+			.init(
+				id: "1",
+				name: "Geometry",
+				image: .init("GeometryTopic"),
+				topDecks: []
+			),
+			.init(
+				id: "2",
+				name: "History",
+				image: .init("GeometryTopic"),
+				topDecks: []
+			),
+			.init(
+				id: "3",
+				name: "Web Dev",
+				image: .init("GeometryTopic"),
+				topDecks: []
+			)
+		]
+		return MainTabView()
+			.environmentObject(currentStore)
 	}
 }
 #endif
