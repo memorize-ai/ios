3916101408e0d4662fa15ec3e2d5dd93f7b2ccab
Ken Mueller <kenmueller0@gmail.com>
2019-11-20 12:02:29 -0800

    top controls market view

--- MarketView.swift
+++ MarketView.swift
@@ -1,8 +1,42 @@
 import SwiftUI
 
 struct MarketView: View {
+	static let deckCellWidth: CGFloat = 165
+	static let numberOfColumns =
+		Int(SCREEN_SIZE.width) / Int(deckCellWidth)
+	static let horizontalCellSpacing: CGFloat = 13
+	static let verticalCellSpacing: CGFloat = 20
+	static let horizontalPadding: CGFloat = 23
+	
+	@EnvironmentObject var currentStore: CurrentStore
+	
+	var grid: some View {
+		Grid(
+			elements: [].map { deck in // TODO: Change this to decks
+				DeckCellWithGetButton(
+					deck: deck,
+					user: currentStore.user,
+					width: Self.deckCellWidth
+				)
+			},
+			columns: Self.numberOfColumns,
+			horizontalSpacing: Self.horizontalCellSpacing,
+			verticalSpacing: Self.verticalCellSpacing
+		)
+		.frame(maxWidth: SCREEN_SIZE.width - Self.horizontalPadding * 2)
+	}
+	
 	var body: some View {
-		Text("MarketView")
+		ZStack(alignment: .top) {
+			HomeViewTopGradient()
+				.edgesIgnoringSafeArea(.all)
+			VStack(spacing: 20) {
+				MarketViewTopControls()
+					.padding(.horizontal, Self.horizontalPadding)
+				// TODO: Add buttons
+				grid
+			}
+		}
 	}
 }
 
---
+++ memorize.ai/MarketViewTopControls.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct MarketViewTopControls: View {
+	var body: some View {
+		Text("MarketViewTopControls")
+	}
+}
+
+#if DEBUG
+struct MarketViewTopControls_Previews: PreviewProvider {
+	static var previews: some View {
+		MarketViewTopControls()
+	}
+}
+#endif
