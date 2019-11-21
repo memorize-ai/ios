1dfef2fb267f7fa20e5b9dec375527a2c93800f3
Ken Mueller <kenmueller0@gmail.com>
2019-11-21 14:36:12 -0800

    MarketViewFilterPopUpSideBarButton

---
+++ memorize.ai/Supplemental Views/MarketView/MarketViewFilterPopUpSideBarButton.swift
@@ -0,0 +1,68 @@
+import SwiftUI
+
+struct MarketViewFilterPopUpSideBarButton<
+	SelectedIcon: View,
+	UnselectedIcon: View
+>: View {
+	let selectedIcon: () -> SelectedIcon
+	let unselectedIcon: () -> UnselectedIcon
+	let isSelected: Bool
+	let onClick: () -> Void
+	
+	var backgroundColor: Color? {
+		isSelected ? .neonGreen : nil
+	}
+	
+	var body: some View {
+		VStack {
+			Button(action: onClick) {
+				ZStack {
+					backgroundColor
+					if isSelected {
+						selectedIcon()
+					} else {
+						unselectedIcon()
+					}
+				}
+			}
+			.frame(height: 50)
+			if !isSelected {
+				Rectangle()
+					.foregroundColor(literal: #colorLiteral(red: 0.8470588235, green: 0.8470588235, blue: 0.8470588235, alpha: 1))
+					.frame(height: 1)
+			}
+		}
+		.frame(width: 50)
+	}
+}
+
+#if DEBUG
+struct MarketViewFilterPopUpSideBarButton_Previews: PreviewProvider {
+	static var previews: some View {
+		VStack {
+			MarketViewFilterPopUpSideBarButton(
+				selectedIcon: {
+					Image.grayDownloadIcon // TODO: Change to white download icon
+						.resizable()
+				},
+				unselectedIcon: {
+					Image.grayDownloadIcon
+						.resizable()
+				},
+				isSelected: true
+			) {}
+			MarketViewFilterPopUpSideBarButton(
+				selectedIcon: {
+					Image.grayDownloadIcon // TODO: Change to white download icon
+						.resizable()
+				},
+				unselectedIcon: {
+					Image.grayDownloadIcon
+						.resizable()
+				},
+				isSelected: false
+			) {}
+		}
+	}
+}
+#endif
