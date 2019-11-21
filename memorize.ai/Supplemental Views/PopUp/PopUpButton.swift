663d0f42fe94234ea28659255f157429eb690863
Ken Mueller <kenmueller0@gmail.com>
2019-11-21 10:47:40 -0800

    pop up button

---
+++ memorize.ai/Supplemental Views/PopUp/PopUpButton.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+struct PopUpButton<Icon: View>: View {
+	let icon: Icon?
+	let text: String
+	let textColor: Color
+	let onClick: () -> Void
+	
+	var body: some View {
+		Button(action: onClick) {
+			HStack(spacing: 20) {
+				icon
+				Text(text)
+					.font(.muli(.semiBold, size: 17))
+					.foregroundColor(textColor)
+			}
+		}
+	}
+}
+
+#if DEBUG
+struct PopUpButton_Previews: PreviewProvider {
+	static var previews: some View {
+		PopUpButton(
+			icon: XButton(.purple, height: 15),
+			text: "Remove",
+			textColor: .extraPurple
+		) {}
+	}
+}
+#endif
