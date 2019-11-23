a9066ed7d4644824fd4e2db2714519e1fccba884
Ken Mueller <kenmueller0@gmail.com>
2019-11-23 18:08:54 -0500

    able to recieve json

--- memorize.ai/Models/Deck/Deck+Search.swift
+++ memorize.ai/Models/Deck/Deck+Search.swift
@@ -8,10 +8,26 @@ extension Deck {
 		averageRatingGreaterThan ratingFilter: Double?,
 		numberOfDownloadsGreaterThan downloadsFilter: Int?
 	) -> Promise<[Deck]> {
-		.init { seal in
+		var filters = [String: Any]()
+		
+		if let topicsFilter = topicsFilter {
+			filters["topics"] = topicsFilter.map(~\.id)
+		}
+		if let ratingFilter = ratingFilter {
+			filters["average_rating"] = ["from": ratingFilter]
+		}
+		if let downloadsFilter = downloadsFilter {
+			filters["download_count"] = ["from": downloadsFilter]
+		}
+		
+		return .init { seal in
 			AF.request(
 				"\(APP_SEARCH_API_ENDPOINT)/api/as/v1/engines/\(DECKS_ENGINE_NAME)/search",
-				method: .post,
+				method: .get,
+				parameters: [
+					"query": query,
+					"filters": filters
+				],
 				headers: [
 					.init(name: "Content-Type", value: "application/json"),
 					.init(name: "Authorization", value: "Bearer \(DECKS_SEARCH_KEY)")
@@ -19,6 +35,9 @@ extension Deck {
 			).responseJSON { response in
 				switch response.result {
 				case let .success(json):
+					guard let json = json as? [String: Any] else {
+						return seal.reject(UNKNOWN_ERROR)
+					}
 					seal.fulfill(translateSearchResponseJSON(json))
 				case let .failure(error):
 					seal.reject(error)
@@ -27,7 +46,7 @@ extension Deck {
 		}
 	}
 	
-	private static func translateSearchResponseJSON(_ json: Any) -> [Deck] {
+	private static func translateSearchResponseJSON(_ json: [String: Any]) -> [Deck] {
 		print("JSON_RESPONSE:", json)
 		return []
 	}
