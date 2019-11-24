188a7494d4bc4152c73c2101200a609e267c7934
Ken Mueller <kenmueller0@gmail.com>
2019-11-24 16:07:58 -0500

    text

--- memorize.ai/Supplemental Views/DecksView/DecksViewTopControls.swift
+++ memorize.ai/Supplemental Views/DecksView/DecksViewTopControls.swift
@@ -8,25 +8,30 @@ struct DecksViewTopControls: View {
 			ShowSideBarButton {
 				HamburgerMenu()
 			}
-			if currentStore.selectedDeck!.hasImage {
-				Group {
-					if currentStore.selectedDeck!.image == nil {
-						Color.white
-							.opacity(0.5)
-							.frame(width: 40, height: 40)
-					} else {
-						currentStore.selectedDeck!.image!
-							.resizable()
-							.aspectRatio(contentMode: .fill)
-							.frame(width: 40, height: 40)
-							.clipped()
+			HStack(spacing: 10) {
+				if currentStore.selectedDeck!.hasImage {
+					Group {
+						if currentStore.selectedDeck!.image == nil {
+							Color.white
+								.opacity(0.5)
+								.frame(width: 40, height: 40)
+						} else {
+							currentStore.selectedDeck!.image!
+								.resizable()
+								.aspectRatio(contentMode: .fill)
+								.frame(width: 40, height: 40)
+								.clipped()
+						}
 					}
+					.cornerRadius(5)
 				}
-				.cornerRadius(5)
+				Text(currentStore.selectedDeck!.name)
+					.font(.muli(.bold, size: 17))
+					.foregroundColor(.white)
 			}
 			Spacer()
 			VerticalTripleDots {
-				
+				// TODO: Show options pop-up
 			}
 		}
 	}
