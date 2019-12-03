d3949889ffc9dddae2f97fe2a5fbadc8b7572af3
Ken Mueller <kenmueller0@gmail.com>
2019-12-02 17:39:23 -0800

    text panel

--- memorize.ai/Supplemental Views/MarketDeckView/MarketDeckViewInfoPanels.swift
+++ memorize.ai/Supplemental Views/MarketDeckView/MarketDeckViewInfoPanels.swift
@@ -4,9 +4,13 @@ struct MarketDeckViewInfoPanels: View {
 	@EnvironmentObject var deck: Deck
 	
 	var divider: some View {
-		Rectangle()
-			.foregroundColor(literal: #colorLiteral(red: 0.8078431373, green: 0.8078431373, blue: 0.8078431373, alpha: 0.5))
-			.frame(width: 1, height: 28)
+		Group {
+			Spacer()
+			Rectangle()
+				.foregroundColor(literal: #colorLiteral(red: 0.8078431373, green: 0.8078431373, blue: 0.8078431373, alpha: 0.5))
+				.frame(width: 1.5, height: 28)
+			Spacer()
+		}
 	}
 	
 	var ratingPanel: some View {
@@ -14,11 +18,17 @@ struct MarketDeckViewInfoPanels: View {
 	}
 	
 	func textPanel(content: String, description: String) -> some View {
-		EmptyView() // TODO: Add content
+		VStack {
+			Text(content)
+			Text(description)
+		}
+		.font(.muli(.bold, size: 17))
+		.foregroundColor(.white)
 	}
 	
 	var body: some View {
 		HStack {
+			Spacer()
 			ratingPanel
 			divider
 			textPanel(
@@ -30,7 +40,9 @@ struct MarketDeckViewInfoPanels: View {
 				content: deck.numberOfCards.formatted,
 				description: "Card\(deck.numberOfCards == 1 ? "" : "s")"
 			)
+			Spacer()
 		}
+		.padding(.horizontal, 23)
 		.frame(height: 30)
 	}
 }
