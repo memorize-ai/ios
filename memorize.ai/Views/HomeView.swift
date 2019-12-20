8c071f68f82598c67a2da3e11401db2df63254e2
Ken Mueller <kenmueller0@gmail.com>
2019-12-19 19:24:48 -0800

    create deck button formatting

--- memorize.ai/Views/HomeView.swift
+++ memorize.ai/Views/HomeView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct HomeView: View {
 	@EnvironmentObject var currentStore: CurrentStore
-		
+	
 	var body: some View {
 		GeometryReader { geometry in
 			ZStack(alignment: .top) {
@@ -15,8 +15,23 @@ struct HomeView: View {
 						.padding(.horizontal, 23)
 					ScrollView {
 						PublishDeckViewNavigationLink {
-							Text("Create deck") // TODO: Add formatting
+							CustomRectangle(
+								background: Color.white
+							) {
+								HStack(spacing: 3) {
+									Image(systemName: .plus)
+										.font(Font.title.weight(.bold))
+										.scaleEffect(0.65)
+									Text("CREATE DECK")
+										.font(.muli(.extraBold, size: 16))
+								}
+								.foregroundColor(.darkBlue)
+								.padding(.leading, 12)
+								.padding(.trailing)
+								.padding(.vertical, 10)
+							}
 						}
+						.padding(.bottom, 6)
 						HomeViewPerformanceCard(
 							currentUser: self.currentStore.user
 						)
