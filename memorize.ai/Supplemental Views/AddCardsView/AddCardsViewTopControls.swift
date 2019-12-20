741c1583a730fc6769d2c8470ac9bcdfd461ccbf
Ken Mueller <kenmueller0@gmail.com>
2019-12-20 00:12:58 -0800

    top controls

--- memorize.ai/Supplemental Views/AddCardsView/AddCardsViewTopControls.swift
+++ memorize.ai/Supplemental Views/AddCardsView/AddCardsViewTopControls.swift
@@ -1,8 +1,46 @@
 import SwiftUI
 
 struct AddCardsViewTopControls: View {
+	@Environment(\.presentationMode) var presentationMode
+	
+	@EnvironmentObject var model: AddCardsViewModel
+	
+	func goBack() {
+		presentationMode.wrappedValue.dismiss()
+	}
+	
 	var body: some View {
-		Text("Hello, World!")
+		HStack(spacing: 22) {
+			Button(action: goBack) {
+				XButton(.transparentWhite, height: 18)
+			}
+			Text("Add cards")
+				.font(.muli(.bold, size: 20))
+				.foregroundColor(.white)
+			Spacer()
+			Button(action: model.publish) {
+				CustomRectangle(
+					background: Color.transparent,
+					borderColor: .transparentLightGray,
+					borderWidth: 1.5
+				) {
+					Group {
+						if model.publishLoadingState.isLoading {
+							ActivityIndicator()
+								.padding(.horizontal, 25)
+						} else {
+							Text("PUBLISH")
+								.font(.muli(.bold, size: 17))
+								.foregroundColor(Color.white.opacity(0.7))
+								.padding(.horizontal, 10)
+						}
+					}
+					.frame(height: 30)
+				}
+				.opacity(model.isPublishButtonDisabled ? 0.5 : 1)
+			}
+			.disabled(model.isPublishButtonDisabled)
+		}
 	}
 }
 
@@ -10,6 +48,9 @@ struct AddCardsViewTopControls: View {
 struct AddCardsViewTopControls_Previews: PreviewProvider {
 	static var previews: some View {
 		AddCardsViewTopControls()
+			.environmentObject(AddCardsViewModel(
+				deck: PREVIEW_CURRENT_STORE.user.decks.first!
+			))
 	}
 }
 #endif
--- memorize.ai/View Models/AddCardsViewModel.swift
+++ memorize.ai/View Models/AddCardsViewModel.swift
@@ -1,11 +1,22 @@
 import Combine
+import LoadingState
 
 final class AddCardsViewModel: ViewModel {
 	let deck: Deck
 	
 	@Published var cards = [Card]()
 	
+	@Published var publishLoadingState = LoadingState()
+	
 	init(deck: Deck) {
 		self.deck = deck
+	}
+	
+	var isPublishButtonDisabled: Bool {
+		false // TODO: Change this
+	}
+	
+	func publish() {
+		// TODO: Publish cards
 	}
 }
