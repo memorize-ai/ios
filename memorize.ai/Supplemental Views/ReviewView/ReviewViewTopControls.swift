ee79f33aaeb488e08a6df182b3c3bf3f9cf6cfa0
Ken Mueller <kenmueller0@gmail.com>
2020-01-06 11:46:01 -0800

    base for review view

---
+++ memorize.ai/Supplemental Views/ReviewView/ReviewViewTopControls.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct ReviewViewTopControls: View {
+	var body: some View {
+		Text("Hello, World!")
+	}
+}
+
+#if DEBUG
+struct ReviewViewTopControls_Previews: PreviewProvider {
+	static var previews: some View {
+		ReviewViewTopControls()
+	}
+}
+#endif
---
+++ memorize.ai/Views/ReviewView.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+struct ReviewView: View {
+	let numberOfDueCards: Int
+	
+	var body: some View {
+		GeometryReader { geometry in
+			ZStack(alignment: .top) {
+				Group {
+					Color.lightGrayBackground
+					HomeViewTopGradient(
+						addedHeight: geometry.safeAreaInsets.top
+					)
+				}
+				.edgesIgnoringSafeArea(.all)
+				VStack {
+					ReviewViewTopControls()
+						.padding(.horizontal, 23)
+				}
+			}
+		}
+	}
+}
+
+#if DEBUG
+struct ReviewView_Previews: PreviewProvider {
+	static var previews: some View {
+		ReviewView(numberOfDueCards: 5)
+	}
+}
+#endif
