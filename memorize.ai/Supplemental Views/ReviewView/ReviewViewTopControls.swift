327b2ab99e08fe77b9e8cb0dd99ee74a9c20e8b7
Ken Mueller <kenmueller0@gmail.com>
2020-01-06 11:55:49 -0800

    top controls

--- memorize.ai/Supplemental Views/ReviewView/ReviewViewTopControls.swift
+++ memorize.ai/Supplemental Views/ReviewView/ReviewViewTopControls.swift
@@ -1,8 +1,39 @@
 import SwiftUI
 
 struct ReviewViewTopControls: View {
+	@Environment(\.presentationMode) var presentationMode
+	
+	var title: String {
+		"1 / 50" // TODO: Change this
+	}
+	
 	var body: some View {
-		Text("Hello, World!")
+		HStack(spacing: 22) {
+			Button(action: {
+				self.presentationMode.wrappedValue.dismiss()
+			}) {
+				XButton(.transparentWhite, height: 18)
+			}
+			Text(title)
+				.font(.muli(.bold, size: 20))
+				.foregroundColor(.white)
+			Spacer()
+			Button(action: {
+				// TODO: Skip card
+			}) {
+				CustomRectangle(
+					background: Color.transparent,
+					borderColor: .transparentLightGray,
+					borderWidth: 1.5
+				) {
+					Text("SKIP")
+						.font(.muli(.bold, size: 17))
+						.foregroundColor(Color.white.opacity(0.7))
+						.padding(.horizontal, 10)
+						.frame(height: 30)
+				}
+			}
+		}
 	}
 }
 
