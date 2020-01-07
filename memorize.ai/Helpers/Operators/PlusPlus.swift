9fff4ba58a59571fcbc26005d36dad83c1dee7d7
Ken Mueller <kenmueller0@gmail.com>
2020-01-07 15:34:41 -0800

    plus plus

--- memorize.ai/Helpers/Operators/PlusPlus.swift
+++ memorize.ai/Helpers/Operators/PlusPlus.swift
@@ -1,5 +1,15 @@
+prefix operator ++
+
+@discardableResult
+prefix func ++ <T: Numeric>(num: inout T) -> T {
+	num += 1
+	return num
+}
+
 postfix operator ++
 
-postfix func ++ <T: Numeric>(num: inout T) {
+@discardableResult
+postfix func ++ <T: Numeric>(num: inout T) -> T {
 	num += 1
+	return num - 1
 }
