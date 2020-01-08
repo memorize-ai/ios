8645181c20af090f9bc32ad5b28650e59935354b
Ken Mueller <kenmueller0@gmail.com>
2020-01-07 20:46:49 -0800

    card css

--- memorize.ai/Models/Card/Card+HTML.swift
+++ memorize.ai/Models/Card/Card+HTML.swift
@@ -1,50 +1,15 @@
 import HTML
 
 extension Card {
-	private static let css = #"""
-	body {
-		padding-top: 36px;
-		font-family: sans-serif;
-		font-size: 45px;
-	}
-	.fr-text-gray {
-		color: #aaa;
-	}
-	.fr-text-bordered {
-		border-top: solid 1px #222;
-		border-bottom: solid 1px #222;
-		padding: 10px 0;
-	}
-	.fr-text-spaced {
-		letter-spacing: 1px;
-	}
-	.fr-text-uppercase {
-		text-transform: uppercase;
-	}
-	.fr-class-highlighted {
-		background-color: #ff0;
-	}
-	.fr-class-code {
-		border-color: #ccc;
-		border-radius: 2px;
-		-moz-border-radius: 2px;
-		-webkit-border-radius: 2px;
-		-moz-background-clip: padding;
-		-webkit-background-clip: padding-box;
-		background-clip: padding-box;
-		background: #f5f5f5;
-		padding: 10px;
-		font-family: 'Courier New', Courier, monospace;
-	}
-	.fr-class-transparency {
-		opacity: 0.5;
-	}
-	"""#
-	
 	private static func htmlWithText(_ text: String) -> HTMLElement {
 		HTMLElement.html
 			.child {
 				HTMLElement.head
+					.child {
+						HTMLElement.link
+							.rel("stylesheet")
+							.href("froala.css")
+					}
 					.child {
 						HTMLElement.link
 							.rel("stylesheet")
@@ -61,13 +26,13 @@ extension Card {
 							.src("auto-render.js")
 							.onLoad("renderMathInElement(document.body)")
 					}
-					.child {
-						HTMLElement.style
-							.child(css)
-					}
 			}
 			.child {
 				HTMLElement.body
+					.class("fr-view")
+					.fontFamily("sans-serif")
+					.fontSize("45px")
+					.margin("20px 30px")
 					.child(text)
 			}
 	}
