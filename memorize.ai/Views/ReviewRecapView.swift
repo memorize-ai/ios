b7a0f37da2fdfaee4553e5ca0aad358444295332
Ken Mueller <kenmueller0@gmail.com>
2020-01-08 16:34:29 -0800

    added review recap

---
+++ memorize.ai/Views/ReviewRecapView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct ReviewRecapView: View {
+	var body: some View {
+		Text("Hello, World!")
+	}
+}
+
+#if DEBUG
+struct ReviewRecapView_Previews: PreviewProvider {
+	static var previews: some View {
+		ReviewRecapView()
+	}
+}
+#endif
