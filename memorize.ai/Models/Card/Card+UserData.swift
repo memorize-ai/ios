bb92a489217a7f3135337ead20f52b579760b38b
Ken Mueller <kenmueller0@gmail.com>
2020-01-14 11:16:12 -0800

    user data section id

--- memorize.ai/Models/Card/Card+UserData.swift
+++ memorize.ai/Models/Card/Card+UserData.swift
@@ -4,22 +4,30 @@ import FirebaseFirestore
 extension Card {
 	struct UserData: Hashable {
 		var isNew: Bool
+		var sectionId: String
 		var dueDate: Date
 		
 		#if DEBUG
-		init(isNew: Bool, dueDate: Date) {
+		init(
+			isNew: Bool = true,
+			sectionId: String = "",
+			dueDate: Date = .now
+		) {
 			self.isNew = isNew
+			self.sectionId = sectionId
 			self.dueDate = dueDate
 		}
 		#endif
 		
 		init(snapshot: DocumentSnapshot) {
 			isNew = snapshot.get("new") as? Bool ?? true
+			sectionId = snapshot.get("section") as? String ?? ""
 			dueDate = snapshot.getDate("due") ?? .now
 		}
 		
 		mutating func updateFromSnapshot(_ snapshot: DocumentSnapshot) {
 			isNew = snapshot.get("new") as? Bool ?? true
+			sectionId = snapshot.get("section") as? String ?? ""
 			dueDate = snapshot.getDate("due") ?? .now
 		}
 	}
