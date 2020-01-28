8badb68a8d05912ca7327e7cf105e0f69962c97e
Ken Mueller <kenmueller0@gmail.com>
2020-01-28 08:58:38 -0800

    working on deck performance

--- memorize.ai/Supplemental Views/ReviewRecapView/ReviewRecapViewDeckPerformanceRow.swift
+++ memorize.ai/Supplemental Views/ReviewRecapView/ReviewRecapViewDeckPerformanceRow.swift
@@ -1,11 +1,70 @@
 import SwiftUI
 
 struct ReviewRecapViewDeckPerformanceRow: View {
+	@State var isExpanded = false
+	
 	let rating: Card.PerformanceRating
 	let decks: [Deck]
 	
+	var deckCountMessage: String {
+		let count = decks.count
+		return "(\(count.formatted) deck\(count == 1 ? "" : "s"))"
+	}
+	
+	var minusIcon: some View {
+		Capsule()
+			.foregroundColor(.darkBlue)
+			.frame(height: 2)
+	}
+	
+	var plusIcon: some View {
+		ZStack {
+			minusIcon
+			minusIcon
+				.rotationEffect(.degrees(90))
+		}
+	}
+	
 	var body: some View {
-		Text("ReviewRecapViewDeckPerformanceRow")
+		VStack {
+			HStack {
+				Text("Frequently \(rating.title.lowercased())")
+					.font(.muli(.extraBold, size: 15))
+					.foregroundColor(.darkGray)
+					.layoutPriority(1)
+				Rectangle()
+					.foregroundColor(.lightGrayBorder)
+					.frame(height: 1)
+				Text(deckCountMessage)
+					.font(.muli(.bold, size: 15))
+					.foregroundColor(.darkBlue)
+				Button(action: {
+					self.isExpanded.toggle()
+				}) {
+					ZStack {
+						Circle()
+							.stroke(Color.lightGrayBorder)
+						Group {
+							if isExpanded {
+								minusIcon
+							} else {
+								plusIcon
+							}
+						}
+						.padding(5)
+					}
+					.frame(width: 21, height: 21)
+				}
+			}
+			.padding(.horizontal, 23)
+			if isExpanded && !decks.isEmpty {
+				ForEach(decks) { deck in
+					Text(deck.name)
+						.padding(.horizontal, 23)
+				}
+			}
+		}
+		.animation(.linear(duration: 0.15))
 	}
 }
 
