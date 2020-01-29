9568b869084a44aae671e54b81e161de509d6735
Ken Mueller <kenmueller0@gmail.com>
2020-01-29 12:16:15 -0800

    top of cell

--- memorize.ai/Supplemental Views/ReviewRecapView/ReviewRecapViewCardPerformanceRowCardCell.swift
+++ memorize.ai/Supplemental Views/ReviewRecapView/ReviewRecapViewCardPerformanceRowCardCell.swift
@@ -23,23 +23,29 @@ struct ReviewRecapViewCardPerformanceRowCardCell: View {
 				cornerRadius: 8
 			) {
 				VStack(alignment: .leading) {
-					HStack {
+					if card.isNew {
+						Text("NEW")
+							.font(.muli(.semiBold, size: 13))
+							.foregroundColor(.darkBlue)
+							.padding(.bottom, -4)
+					}
+					HStack(alignment: .bottom) {
 						if shouldShowDeckName {
 							Text(deck.name)
-								.font(.muli(.bold, size: 17))
-								.foregroundColor(.lightGrayText)
-								.alignment(.leading)
-								.padding(.bottom, 8)
+								.font(.muli(.semiBold, size: 13))
+								.foregroundColor(.darkGray)
+								.shrinks()
 							if shouldShowSectionName {
-								Text("|")
+								Rectangle()
+									.foregroundColor(.lightGrayBorder)
+									.frame(width: 1.5, height: 14)
 							}
 						}
 						if shouldShowSectionName {
 							Text(section.name)
-								.font(.muli(.bold, size: 17))
-								.foregroundColor(.lightGrayText)
-								.alignment(.leading)
-								.padding(.bottom, 8)
+								.font(.muli(.semiBold, size: 13))
+								.foregroundColor(.darkGray)
+								.shrinks()
 						}
 						Spacer()
 					}
@@ -77,6 +83,7 @@ struct ReviewRecapViewCardPerformanceRowCardCell: View {
 							.alignment(.leading)
 					}
 					.frame(minHeight: 40, alignment: .top)
+					// TODO: Add bottom content
 				}
 				.padding(8)
 			}
@@ -100,13 +107,18 @@ struct ReviewRecapViewCardPerformanceRowCardCell_Previews: PreviewProvider {
 			deck: PREVIEW_CURRENT_STORE.user.decks[0],
 			section: PREVIEW_CURRENT_STORE.user.decks[0].sections[0],
 			card: PREVIEW_CURRENT_STORE.user.decks[0].previewCards[0],
-			reviewData: .init(
-				parent: PREVIEW_CURRENT_STORE.user.decks[0].previewCards[0],
-				userData: .init()
-			),
+			reviewData: {
+				let reviewData = Card.ReviewData(
+					parent: PREVIEW_CURRENT_STORE.user.decks[0].previewCards[0],
+					userData: .init()
+				)
+				reviewData.rating = .easy
+				return reviewData
+			}(),
 			shouldShowDeckName: true,
 			shouldShowSectionName: true
 		)
+		.padding(.horizontal)
 	}
 }
 #endif
