5ff58c87fec6edf544cd0800486f2f5b70206aae
Ken Mueller <kenmueller0@gmail.com>
2020-02-09 19:55:30 -0800

    removed match

--- memorize.ai/Extensions/String.swift
+++ memorize.ai/Extensions/String.swift
@@ -18,16 +18,4 @@ extension String {
 		formatter.dateFormat = format
 		return formatter.date(from: self)
 	}
-	
-	func match(_ regex: String) -> [[String]] {
-		let nsString = self as NSString
-		return (try? NSRegularExpression(pattern: regex, options: []))?
-			.matches(in: self, options: [], range: NSMakeRange(0, count)).map { match in
-				(0..<match.numberOfRanges).map {
-					match.range(at: $0).location == NSNotFound
-						? ""
-						: nsString.substring(with: match.range(at: $0))
-				}
-			} ?? []
-	}
 }
