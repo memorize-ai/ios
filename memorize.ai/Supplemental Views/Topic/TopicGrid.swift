f09a15a3dbaa19f20a331592d4af79f838e0cd1d
Ken Mueller <kenmueller0@gmail.com>
2020-08-03 00:47:05 -0700

    topic grid

---
+++ memorize.ai/Supplemental Views/Topic/TopicGrid.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+struct TopicGrid: View {
+	static let spacing: CGFloat = 8
+	static let columns = [
+		GridItem(.adaptive(minimum: TopicCell.dimension))
+	]
+	
+	let topics: [Topic]
+	
+	let isSelected: (Topic) -> Bool
+	let toggleSelect: (Topic) -> Void
+	
+	var body: some View {
+		LazyVGrid(columns: Self.columns, spacing: Self.spacing) {
+			ForEach(topics) { topic in
+				TopicCell(
+					topic: topic,
+					isSelected: isSelected(topic),
+					toggleSelect: { toggleSelect(topic) }
+				)
+			}
+		}
+	}
+}
+
+#if DEBUG
+struct TopicGrid_Previews: PreviewProvider {
+	static var previews: some View {
+		TopicGrid(
+			topics: [],
+			isSelected: { _ in true },
+			toggleSelect: { _ in }
+		)
+	}
+}
+#endif
